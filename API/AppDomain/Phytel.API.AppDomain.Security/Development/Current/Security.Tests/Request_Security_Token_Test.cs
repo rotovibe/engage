275becbc81e488c74ab0c3adefe9c559f5a2ba21
Mel Bobadilla <mel.bobadilla@phytel.com>
2013-11-05 16:58:05 +0000

    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Services;C102

--- API/AppDomain/Phytel.API.AppDomain.Security/Development/Current/Security.Tests/Request_Security_Token_Test.cs
+++ API/AppDomain/Phytel.API.AppDomain.Security/Development/Current/Security.Tests/Request_Security_Token_Test.cs
@@ -12,12 +12,13 @@ namespace Phytel.API.AppDomain.Security.Services.Test
         [TestMethod]
         public void ValidateCredential_Test()
         {
-            string controlValue = "tdigiorgio@phytel.com";
+            // need to make sure that there is a registration in the apiusertoken table in the c3 DB for this user.
+            string controlValue = "inhealthadmin";
             string sampleValue;
             IRestClient client = new JsonServiceClient();
 
             AuthenticateResponse response = client.Post<AuthenticateResponse>("http://localhost:999/api/security/login",
-                new AuthenticateRequest { APIKey = "12345", Product = "NG", Token = "abcxyz" } as object);
+                new AuthenticateRequest { APIKey = "12345", Product = "NG", Token = "1234" } as object);
             sampleValue = response.UserName;
             Assert.AreEqual(controlValue, sampleValue);
         }
