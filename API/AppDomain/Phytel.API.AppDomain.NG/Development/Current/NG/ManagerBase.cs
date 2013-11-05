0d6edfe09cb59bfbb443d90ab4a63fb251708f5b
Mel Bobadilla <mel.bobadilla@phytel.com>
2013-11-05 23:34:36 +0000

    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Services;C110

---
+++ API/AppDomain/Phytel.API.AppDomain.NG/Development/Current/NG/ManagerBase.cs
@@ -0,0 +1,27 @@
+﻿using Phytel.API.AppDomain.Security.DTO;
+using ServiceStack.Service;
+using ServiceStack.ServiceClient.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phytel.API.AppDomain.NG
+{
+    public abstract class ManagerBase
+    {
+        public bool IsUserValidated(string token)
+        {
+            bool result = false;
+            IRestClient client = new JsonServiceClient();
+
+            ValidateTokenResponse response = client.Post<ValidateTokenResponse>("http://localhost:999/api/security/Token",
+                new ValidateTokenRequest { Token = token } as object);
+
+            if (response.IsValid) result = true;
+
+            return result;
+        }
+    }
+}
