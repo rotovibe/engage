81901cd5695b10b7548ca3f21920a4db05b3d76c
Mel Bobadilla <mel.bobadilla@phytel.com>
2013-11-05 23:40:55 +0000

    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Services;C112

--- API/AppDomain/Phytel.API.AppDomain.NG/Development/Current/NG.Services/NGService.cs
+++ API/AppDomain/Phytel.API.AppDomain.NG/Development/Current/NG.Services/NGService.cs
@@ -1,5 +1,6 @@
 using Phytel.API.AppDomain.NG.DTO;
 using System;
+using System.Net;
 
 namespace Phytel.API.AppDomain.NG.Service
 {
@@ -7,22 +8,25 @@ namespace Phytel.API.AppDomain.NG.Service
     {
         public object Post(PatientRequest request)
         {
+            bool validated = false;
             PatientResponse response = new PatientResponse();
             try
             {
                 //request.Token = base.Request.Headers["APIToken"] as string;
 
-                // validate user against apiuser datastore
-                bool result = NGManager.IsUserValidated(request.Token);
+                // if the token is valid, go ahead and grant the request.
+                NGManager ngm = new NGManager();
+                response = ngm.GetPatientByID(request.Token, request.ID, request.Product, request.ContractNumber, out validated);
 
-                if (result)
+                if (!validated)
                 {
-                    response = NGManager.GetPatientByID(request.ID, request.Product, request.ContractNumber);
+                    base.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
                 }
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 //TODO: Log this to C3 database via ASE
+                base.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
                 response.Status = new ServiceStack.ServiceInterface.ServiceModel.ResponseStatus("Excepton", ex.Message);
             }
             return response;
