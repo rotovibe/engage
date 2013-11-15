a14cdb453986b905799285504de1f4d39bbd9641
Snehal Ingavale <Snehal.Ingavale@phytel.com>
2013-11-15 20:27:22 +0000

    Adding interface that any mongo entity should use to enforce the required fields
    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Services;C211

---
+++ API/Interfaces/Development/Current/Phytel.API.Interface/IMEEntity.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Phytel.API.Interface
+{
+    public interface IMEEntity
+    {
+        [BsonExtraElements]
+        [BsonIgnoreIfNull(true)]
+        [BsonElement("ex")]
+        Dictionary<string, object> ExtraElements { get; set; }
+
+        [BsonElement("v")]
+        [BsonIgnoreIfNull(true)]
+        string Version { get; set; }
+
+        [BsonElement( "uby")]
+        [BsonIgnoreIfNull(true)]
+        string UpdatedBy { get; set; }
+
+        [BsonElement("del")]
+        [BsonDefaultValue(0)]
+        [BsonIgnoreIfNull(true)]
+        bool DeleteFlag { get; set; }
+
+        [BsonElement("ttl")]
+        [BsonDefaultValue(null)]
+        [BsonIgnoreIfNull(true)]
+        DateTime TTLDate { get; set; }
+
+        [BsonIgnoreIfNull(true)]
+        [BsonElement("uon")]
+        DateTime LastUpdatedOn { get; set; }
+    }
+}
