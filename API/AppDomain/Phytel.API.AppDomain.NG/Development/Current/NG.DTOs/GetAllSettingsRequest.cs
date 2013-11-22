27404ab59b01f8626b5d465717980b365a708745
Snehal Ingavale <Snehal.Ingavale@phytel.com>
2013-11-22 23:45:19 +0000

    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Services;C307

---
+++ API/AppDomain/Phytel.API.AppDomain.NG/Development/Current/NG.DTOs/GetAllSettingsRequest.cs
@@ -0,0 +1,19 @@
+﻿using ServiceStack.ServiceHost;
+using Phytel.API.Interface;
+using System.Collections.Generic;
+
+namespace Phytel.API.AppDomain.NG.DTO
+{
+    [Route("/{Version}/{ContractNumber}/settings", "GET")]
+    public class GetAllSettingsRequest : IAppDomainRequest
+    {
+        [ApiMember(Name = "ContractNumber", Description = "Contract parameter will be defined in the route.", ParameterType = "path", DataType = "string", IsRequired = true)]
+        public string ContractNumber { get; set; }
+
+        [ApiMember(Name = "Token", Description = "Token parameter in Header", ParameterType = "path", DataType = "string", IsRequired = true)]
+        public string Token { get; set; }
+
+        [ApiMember(Name = "Version", Description = "Version of the Request", ParameterType = "path", DataType = "string", IsRequired = true)]
+        public string Version { get; set; }
+    }
+}
