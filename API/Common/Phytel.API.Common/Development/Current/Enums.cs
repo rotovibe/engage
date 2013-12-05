9a7cae62616b8649875ee2f71d016f5c8001a781
Snehal Ingavale <Snehal.Ingavale@phytel.com>
2013-12-05 20:28:38 +0000

    Adding Status in Enum class
    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Services;C385

--- API/Common/Phytel.API.Common/Development/Current/Enums.cs
+++ API/Common/Phytel.API.Common/Development/Current/Enums.cs
@@ -4,12 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Phytel.API.Common.Audit
+namespace Phytel.API.Common
 {
     public enum DispatchType
     {
         auditerror,
         auditaction,
         auditview
+    }
+
+    public enum Status
+    {
+        Active = 1,
+        Inactive = 2,
+        InReview = 3,
+        Met = 4,
+        NotMet = 5
     }
 }
