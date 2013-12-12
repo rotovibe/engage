93eee67010c8c2ee0efeb25e1e4fb22409a2d0b2
Snehal Ingavale <Snehal.Ingavale@phytel.com>
2013-12-12 23:08:03 +0000

    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Services;C459

--- API/DataDomain/Phytel.API.DataDomain.LookUp/Development/Current/LookUp.DTO/GetProblemDataRequest.cs
+++ API/DataDomain/Phytel.API.DataDomain.LookUp/Development/Current/LookUp.DTO/GetProblemDataRequest.cs
@@ -6,13 +6,13 @@ namespace Phytel.API.DataDomain.LookUp.DTO
     [Route("/{Context}/{Version}/{ContractNumber}/problem/{ProblemID}", "GET")]
     public class GetProblemDataRequest : IDataDomainRequest
     {
-        [ApiMember(Name = "ProblemID", Description = "ID of the problem being requested", ParameterType = "property", DataType = "string", IsRequired = false)]
+        [ApiMember(Name = "ProblemID", Description = "ID of the problem being requested", ParameterType = "path", DataType = "string", IsRequired = true)]
         public string ProblemID { get; set; }
 
-        [ApiMember(Name = "Context", Description = "Context", ParameterType = "body", DataType = "string", IsRequired = false)]
+        [ApiMember(Name = "Context", Description = "Context", ParameterType = "body", DataType = "string", IsRequired = true)]
         public string Context { get; set; }
 
-        [ApiMember(Name = "ContractNumber", Description = "Contract Number to retrieve data from", ParameterType = "path", DataType = "string", IsRequired = false)]
+        [ApiMember(Name = "ContractNumber", Description = "Contract Number to retrieve data from", ParameterType = "path", DataType = "string", IsRequired = true)]
         public string ContractNumber { get; set; }
 
         [ApiMember(Name = "Version", Description = "Version of the Data Domain API being called", ParameterType = "path", DataType = "string", IsRequired = true)]
