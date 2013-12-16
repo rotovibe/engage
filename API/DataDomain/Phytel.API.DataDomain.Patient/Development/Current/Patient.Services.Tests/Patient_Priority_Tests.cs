9322bb00e200d7678d011e652e1c54fe6225aa66
Mel Bobadilla <mel.bobadilla@phytel.com>
2013-12-16 19:59:26 +0000

    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Services;C484

---
+++ API/DataDomain/Phytel.API.DataDomain.Patient/Development/Current/Patient.Services.Tests/Patient_Priority_Tests.cs
@@ -0,0 +1,27 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Phytel.API.DataDomain.Patient.DTO;
+using ServiceStack.Service;
+using ServiceStack.ServiceClient.Web;
+
+namespace Phytel.API.DataDomain.Patient.Service.Test
+{
+    [TestClass]
+    public class User_Services_Test
+    {
+        [TestMethod]
+        public void GetPatientByID()
+        {
+            // http://localhost:8888/Patient/NG/v1/InHealth001/patient/999/priority/?priority=2
+            string patientID = "528f6dc2072ef708ecd90e3a";
+            string contractNumber = "InHealth001";
+            string context ="NG";
+            string priority = "2";
+            string version = "v1";
+            IRestClient client = new JsonServiceClient();
+
+            GetPatientDataResponse response = client.Put<GetPatientDataResponse>(
+                string.Format(@"http://localhost:8888/Patient/{0}/{1}/{2}/patient/{3}/priority/?priority={4}", context, version, contractNumber, patientID, priority ),
+                new GetPatientDataRequest { } as object);
+        }
+    }
+}
