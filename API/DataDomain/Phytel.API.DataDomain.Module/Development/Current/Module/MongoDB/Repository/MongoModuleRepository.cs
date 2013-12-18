45d07dfa07b8e4d1620a59ec8303a6a612e3b3d0
Mel Bobadilla <mel.bobadilla@phytel.com>
2013-12-18 15:42:38 +0000

    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Services;C566

--- API/DataDomain/Phytel.API.DataDomain.Module/Development/Current/Module/MongoDB/Repository/MongoModuleRepository.cs
+++ API/DataDomain/Phytel.API.DataDomain.Module/Development/Current/Module/MongoDB/Repository/MongoModuleRepository.cs
@@ -22,12 +22,12 @@ namespace Phytel.API.DataDomain.Module
             _dbName = contractDBName;
         }
 
-        public T Insert(T newEntity)
+        public object Insert(T newEntity)
         {
             throw new NotImplementedException();
         }
 
-        public T InsertAll(List<T> entities)
+        public object InsertAll(List<T> entities)
         {
             throw new NotImplementedException();
         }
@@ -62,17 +62,17 @@ namespace Phytel.API.DataDomain.Module
             return module;
         }
 
-        public Tuple<string, IQueryable<T>> Select(Interface.APIExpression expression)
+        public Tuple<string, IEnumerable<object>> Select(Interface.APIExpression expression)
         {
             throw new NotImplementedException();
         }
 
-        public IQueryable<T> SelectAll()
+        public IEnumerable<object> SelectAll()
         {
             throw new NotImplementedException();
         }
 
-        public T Update(T entity)
+        public object Update(T entity)
         {
             throw new NotImplementedException();
         }
