d58b5620dc463e23aa8f1316ccaa424b00025c22
Snehal Ingavale <Snehal.Ingavale@phytel.com>
2014-02-17 23:26:49 +0000

    adding customattributes in tasks
    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Services;C1130

--- API/DataDomain/Phytel.API.DataDomain.PatientGoal/Development/Current/PatientGoal.DTO/PatientTaskData.cs
+++ API/DataDomain/Phytel.API.DataDomain.PatientGoal/Development/Current/PatientGoal.DTO/PatientTaskData.cs
@@ -18,5 +18,6 @@ namespace Phytel.API.DataDomain.PatientGoal.DTO
         public string Description { get; set; }
         public DateTime? StatusDate { get; set; }
         public DateTime? StartDate { get; set; }
+        public List<CustomAttributeData> CustomAttributes { get; set; }
     }
 }
