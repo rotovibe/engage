8552518b69e6f48297220f46e4120c85e5fe1f45
Tony DiGiorgio <tony.digiorgio@phytel.com>
2014-02-20 16:39:13 +0000

    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Services;C1179

--- API/Database/PhytelNG/Development/Current/PhytelNG/dbo/Stored Procedures/spPhy_InsertAuditType.sql
+++ API/Database/PhytelNG/Development/Current/PhytelNG/dbo/Stored Procedures/spPhy_InsertAuditType.sql
@@ -2,6 +2,7 @@
 * Author: Brent Giesler
 * Date: 01/17/2014
 * Desc: Procedure inserts new audit types if they don't already exist.
+* Just updating this for testing...
 *******/
 CREATE PROCEDURE [dbo].[spPhy_InsertAuditType] 
     @TypeName varchar(50)
