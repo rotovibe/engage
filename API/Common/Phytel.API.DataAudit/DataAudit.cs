c6b347585906d6f9a2a57d61a0bf75ae34fa55b9
Tony DiGiorgio <tony.digiorgio@phytel.com>
2014-03-03 02:52:38 +0000

    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Services;C1319

---
+++ API/Common/Phytel.API.DataAudit/DataAudit.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phytel.API.DataAudit
+{
+    [Serializable]
+    public class DataAudit
+    {
+        public string UserId { get; set; }
+        public string Type { get; set; }  //Insert, Update, Delete
+        public string EntityType { get; set; }
+        public string EntityID { get; set; }
+        public object Entity { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public string Contract { get; set; }
+
+
+    }
+}
