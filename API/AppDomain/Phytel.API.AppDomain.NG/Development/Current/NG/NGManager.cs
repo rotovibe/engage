608f53b07f8e1887fde387d4f6a2ee75f50d52d5
Snehal Ingavale <Snehal.Ingavale@phytel.com>
2014-03-11 16:01:16 +0000

    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Services;C1529

--- API/AppDomain/Phytel.API.AppDomain.NG/Development/Current/NG/NGManager.cs
+++ API/AppDomain/Phytel.API.AppDomain.NG/Development/Current/NG/NGManager.cs
@@ -82,7 +82,6 @@ namespace Phytel.API.AppDomain.NG
                         pResponse.Patient.DisplaySystemId = sysResponse.PatientSystem.SystemID;
                         pResponse.Patient.DisplaySystemName = sysResponse.PatientSystem.SystemName;
                         pResponse.Patient.DisplayLabel = sysResponse.PatientSystem.DisplayLabel;
-                        throw new Exception("Just a test");
                     }
                 }
 
