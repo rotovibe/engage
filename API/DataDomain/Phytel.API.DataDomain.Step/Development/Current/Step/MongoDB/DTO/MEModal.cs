b6334bed63f1f6cc611860a2c2e9e5e9ef471587
Mel Bobadilla <mel.bobadilla@phytel.com>
2014-03-12 21:06:06 +0000

    added new step type for modal
    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Services;C1555

---
+++ API/DataDomain/Phytel.API.DataDomain.Step/Development/Current/Step/MongoDB/DTO/MEModal.cs
@@ -0,0 +1,28 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using Phytel.Mongo.Linq;
+using System.ComponentModel;
+using System.Collections.Generic;
+
+namespace Phytel.API.DataDomain.Step.DTO
+{
+    [BsonIgnoreExtraElements(false)]
+    public class MEModal : MEStepBase
+    {
+        public const string TitleProperty = "t";
+        [BsonElement(TitleProperty)]
+        [BsonIgnoreIfNull(true)]
+        public string Title { get; set; }
+
+        public const string TextPromptProperty = "text";
+        [BsonElement(TextPromptProperty)]
+        [BsonIgnoreIfNull(true)]
+        [BsonRequired]
+        public string TextPrompt { get; set; }
+
+        public const string ResponseProperty = "responses";
+        [BsonElement(ResponseProperty)]
+        [BsonIgnoreIfNull(true)]
+        public List<MEResponse> Response { get; set; }
+    }
+}
