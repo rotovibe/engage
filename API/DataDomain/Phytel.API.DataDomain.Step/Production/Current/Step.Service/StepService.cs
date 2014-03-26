5a1d2b1fb77f6f5cfee45f8b2ebb727d125aec29
Tony DiGiorgio <tony.digiorgio@phytel.com>
2014-03-26 04:39:24 +0000

    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Services;C2084

---
+++ API/DataDomain/Phytel.API.DataDomain.Step/Production/Current/Step.Service/StepService.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Net;
+using Phytel.API.DataDomain.Step;
+using Phytel.API.DataDomain.Step.DTO;
+using System.Configuration;
+using Phytel.API.Common.Format;
+using System.Web;
+
+namespace Phytel.API.DataDomain.Step.Service
+{
+    public class StepService : ServiceStack.ServiceInterface.Service
+    {
+        private const string _phytelUserIDToken = "x-Phytel-UserID";
+
+        public GetYesNoStepDataResponse Get(GetYesNoStepDataRequest request)
+        {
+            GetYesNoStepDataResponse response = new GetYesNoStepDataResponse();
+            try
+            {
+                //Get the UserId from the Header and update the request object
+                request.UserId = HttpContext.Current.Request.Headers.Get(_phytelUserIDToken);
+                if (string.IsNullOrEmpty(request.UserId))
+                    throw new UnauthorizedAccessException("StepDD:Get()::Unauthorized Access");
+
+                response = StepDataManager.GetYesNoStepByID(request);
+                response.Version = request.Version;
+            }
+            catch (Exception ex)
+            {
+                CommonFormatter.FormatExceptionResponse(response, base.Response, ex);
+
+                string aseProcessID = ConfigurationManager.AppSettings.Get("ASEProcessID") ?? "0";
+                Common.Helper.LogException(int.Parse(aseProcessID), ex);
+            }
+            return response;
+        }
+
+        public GetTextStepDataResponse Get(GetTextStepDataRequest request)
+        {
+            GetTextStepDataResponse response = new GetTextStepDataResponse();
+            try
+            {
+                //Get the UserId from the Header and update the request object
+                request.UserId = HttpContext.Current.Request.Headers.Get(_phytelUserIDToken);
+                if (string.IsNullOrEmpty(request.UserId))
+                    throw new UnauthorizedAccessException("StepDD:Get()::Unauthorized Access");
+
+                response = StepDataManager.GetTextStepByID(request);
+                response.Version = request.Version;
+            }
+            catch (Exception ex)
+            {
+                CommonFormatter.FormatExceptionResponse(response, base.Response, ex);
+
+                string aseProcessID = ConfigurationManager.AppSettings.Get("ASEProcessID") ?? "0";
+                Common.Helper.LogException(int.Parse(aseProcessID), ex);
+            }
+            return response;
+        }
+    }
+}
