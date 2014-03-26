6bcd0165a5556e5416e1697d7ffab629e23de9fc
Tony DiGiorgio <tony.digiorgio@phytel.com>
2014-03-26 04:39:23 +0000

    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Services;C2083

---
+++ API/DataDomain/Phytel.API.DataDomain.Program/Production/Current/Program.DTO/GetContractProgramRequest.cs
@@ -0,0 +1,24 @@
+using Phytel.API.Interface;
+using ServiceStack.ServiceHost;
+
+namespace Phytel.API.DataDomain.Program.DTO
+{
+    [Route("/{Context}/{Version}/{ContractNumber}/ContractProgram/{ContractProgramID}/", "GET")]
+    public class GetContractProgramRequest : IDataDomainRequest
+    {
+        [ApiMember(Name = "UserId", Description = "UserId of the logged in user", ParameterType = "property", DataType = "string", IsRequired = false)]
+        public string UserId { get; set; }
+
+        [ApiMember(Name = "ContractProgramID", Description = "ID of the Program being requested", ParameterType = "property", DataType = "string", IsRequired = false)]
+        public string ContractProgramID { get; set; }
+
+        [ApiMember(Name = "Context", Description = "Product Context requesting the Program", ParameterType = "property", DataType = "string", IsRequired = false)]
+        public string Context { get; set; }
+
+        [ApiMember(Name = "ContractNumber", Description = "Contract Number to retrieve data from", ParameterType = "property", DataType = "string", IsRequired = false)]
+        public string ContractNumber { get; set; }
+
+        [ApiMember(Name = "Version", Description = "Version of the API being called", ParameterType = "property", DataType = "double", IsRequired = false)]
+        public double Version { get; set; }
+    }
+}
---
+++ API/DataDomain/Phytel.API.DataDomain.Program/Production/Current/Program.Service/ProgramService.cs
@@ -0,0 +1,314 @@
+using System;
+using System.Net;
+using Phytel.API.DataDomain.Program;
+using Phytel.API.DataDomain.Program.DTO;
+using Phytel.API.Common.Format;
+using System.Configuration;
+using System.Web;
+
+namespace Phytel.API.DataDomain.Program.Service
+{
+    public class ProgramService : ServiceStack.ServiceInterface.Service
+    {
+        private const string _phytelUserIDToken = "x-Phytel-UserID";
+
+        public GetProgramResponse Post(GetProgramRequest request)
+        {
+            GetProgramResponse response = new GetProgramResponse();
+            try
+            {
+                //Get the UserId from the Header and update the request object
+                request.UserId = HttpContext.Current.Request.Headers.Get(_phytelUserIDToken);
+                if (string.IsNullOrEmpty(request.UserId))
+                    throw new UnauthorizedAccessException("ProgramDD:Post()::Unauthorized Access");
+
+                response = ProgramDataManager.GetProgramByID(request);
+                response.Version = request.Version;
+            }
+            catch (Exception ex)
+            {
+                CommonFormatter.FormatExceptionResponse(response, base.Response, ex);
+
+                string aseProcessID = ConfigurationManager.AppSettings.Get("ASEProcessID") ?? "0";
+                Common.Helper.LogException(int.Parse(aseProcessID), ex);
+            }
+            return response;
+        }
+
+        public GetAllActiveProgramsResponse Get(GetAllActiveProgramsRequest request)
+        {
+            GetAllActiveProgramsResponse response = new GetAllActiveProgramsResponse();
+            try
+            {
+                //Get the UserId from the Header and update the request object
+                request.UserId = HttpContext.Current.Request.Headers.Get(_phytelUserIDToken);
+                if (string.IsNullOrEmpty(request.UserId))
+                    throw new UnauthorizedAccessException("ProgramDD:Get()::Unauthorized Access");
+
+                response = ProgramDataManager.GetAllActiveContractPrograms(request);
+                response.Version = request.Version;
+            }
+            catch (Exception ex)
+            {
+                CommonFormatter.FormatExceptionResponse(response, base.Response, ex);
+
+                string aseProcessID = ConfigurationManager.AppSettings.Get("ASEProcessID") ?? "0";
+                Common.Helper.LogException(int.Parse(aseProcessID), ex);
+            }
+            return response;
+        }
+
+        public PutProgramToPatientResponse Put(PutProgramToPatientRequest request)
+        {
+            PutProgramToPatientResponse response = new PutProgramToPatientResponse();
+            try
+            {
+                //Get the UserId from the Header and update the request object
+                request.UserId = HttpContext.Current.Request.Headers.Get(_phytelUserIDToken);
+                if (string.IsNullOrEmpty(request.UserId))
+                    throw new UnauthorizedAccessException("ProgramDD:Put()::Unauthorized Access");
+
+                response = ProgramDataManager.PutPatientToProgram(request);
+                response.Version = request.Version;
+            }
+            catch (Exception ex)
+            {
+                CommonFormatter.FormatExceptionResponse(response, base.Response, ex);
+
+                string aseProcessID = ConfigurationManager.AppSettings.Get("ASEProcessID") ?? "0";
+                Common.Helper.LogException(int.Parse(aseProcessID), ex);
+            }
+            return response;
+        }
+
+        public PutUpdateResponseResponse Put(PutUpdateResponseRequest request)
+        {
+            PutUpdateResponseResponse response = new PutUpdateResponseResponse();
+            try
+            {
+                //Get the UserId from the Header and update the request object
+                request.UserId = HttpContext.Current.Request.Headers.Get(_phytelUserIDToken);
+                if (string.IsNullOrEmpty(request.UserId))
+                    throw new UnauthorizedAccessException("ProgramDD:Put()::Unauthorized Access");
+
+                response = ProgramDataManager.PutUpdateResponse(request);
+                response.Version = request.Version;
+            }
+            catch (Exception ex)
+            {
+                CommonFormatter.FormatExceptionResponse(response, base.Response, ex);
+
+                string aseProcessID = ConfigurationManager.AppSettings.Get("ASEProcessID") ?? "0";
+                Common.Helper.LogException(int.Parse(aseProcessID), ex);
+            }
+            return response;
+        }
+
+        public PutProgramActionProcessingResponse Put(PutProgramActionProcessingRequest request)
+        {
+            PutProgramActionProcessingResponse response = new PutProgramActionProcessingResponse();
+            try
+            {
+                //Get the UserId from the Header and update the request object
+                request.UserId = HttpContext.Current.Request.Headers.Get(_phytelUserIDToken);
+                if (string.IsNullOrEmpty(request.UserId))
+                    throw new UnauthorizedAccessException("ProgramDD:Put()::Unauthorized Access");
+
+                response = ProgramDataManager.PutProgramActionUpdate(request);
+                response.Version = request.Version;
+            }
+            catch (Exception ex)
+            {
+                CommonFormatter.FormatExceptionResponse(response, base.Response, ex);
+
+                string aseProcessID = ConfigurationManager.AppSettings.Get("ASEProcessID") ?? "0";
+                Common.Helper.LogException(int.Parse(aseProcessID), ex);
+            }
+            return response;
+        }
+
+        public GetProgramResponse Get(GetProgramRequest request)
+        {
+            GetProgramResponse response = new GetProgramResponse();
+            try
+            {
+                //Get the UserId from the Header and update the request object
+                request.UserId = HttpContext.Current.Request.Headers.Get(_phytelUserIDToken);
+                if (string.IsNullOrEmpty(request.UserId))
+                    throw new UnauthorizedAccessException("ProgramDD:Get()::Unauthorized Access");
+
+                response = ProgramDataManager.GetProgramByID(request);
+                response.Version = request.Version;
+            }
+            catch (Exception ex)
+            {
+                CommonFormatter.FormatExceptionResponse(response, base.Response, ex);
+
+                string aseProcessID = ConfigurationManager.AppSettings.Get("ASEProcessID") ?? "0";
+                Common.Helper.LogException(int.Parse(aseProcessID), ex);
+            }
+            return response;
+        }
+
+        public GetProgramDetailsSummaryResponse Get(GetProgramDetailsSummaryRequest request)
+        {
+            GetProgramDetailsSummaryResponse response = new GetProgramDetailsSummaryResponse();
+            try
+            {
+                //Get the UserId from the Header and update the request object
+                request.UserId = HttpContext.Current.Request.Headers.Get(_phytelUserIDToken);
+                if (string.IsNullOrEmpty(request.UserId))
+                    throw new UnauthorizedAccessException("ProgramDD:Get()::Unauthorized Access");
+
+                response = ProgramDataManager.GetPatientProgramDetailsById(request);
+                response.Version = request.Version;
+            }
+            catch (Exception ex)
+            {
+                CommonFormatter.FormatExceptionResponse(response, base.Response, ex);
+
+                string aseProcessID = ConfigurationManager.AppSettings.Get("ASEProcessID") ?? "0";
+                Common.Helper.LogException(int.Parse(aseProcessID), ex);
+            }
+            return response;
+        }
+
+        public GetPatientProgramsResponse Get(GetPatientProgramsRequest request)
+        {
+            GetPatientProgramsResponse response = new GetPatientProgramsResponse();
+            try
+            {
+                //Get the UserId from the Header and update the request object
+                request.UserId = HttpContext.Current.Request.Headers.Get(_phytelUserIDToken);
+                if (string.IsNullOrEmpty(request.UserId))
+                    throw new UnauthorizedAccessException("ProgramDD:Get()::Unauthorized Access");
+
+                response = ProgramDataManager.GetPatientPrograms(request);
+                response.Version = request.Version;
+            }
+            catch (Exception ex)
+            {
+                CommonFormatter.FormatExceptionResponse(response, base.Response, ex);
+
+                string aseProcessID = ConfigurationManager.AppSettings.Get("ASEProcessID") ?? "0";
+                Common.Helper.LogException(int.Parse(aseProcessID), ex);
+            }
+            return response;
+        }
+
+        public GetStepResponseResponse Get(GetStepResponseRequest request)
+        {
+            GetStepResponseResponse response = new GetStepResponseResponse();
+            try
+            {
+                //Get the UserId from the Header and update the request object
+                request.UserId = HttpContext.Current.Request.Headers.Get(_phytelUserIDToken);
+                if (string.IsNullOrEmpty(request.UserId))
+                    throw new UnauthorizedAccessException("ProgramDD:Get()::Unauthorized Access");
+
+                response = ProgramDataManager.GetStepResponse(request);
+                response.Version = request.Version;
+            }
+            catch (Exception ex)
+            {
+                CommonFormatter.FormatExceptionResponse(response, base.Response, ex);
+
+                string aseProcessID = ConfigurationManager.AppSettings.Get("ASEProcessID") ?? "0";
+                Common.Helper.LogException(int.Parse(aseProcessID), ex);
+            }
+            return response;
+        }
+
+        public GetStepResponseListResponse Get(GetStepResponseListRequest request)
+        {
+            GetStepResponseListResponse response = new GetStepResponseListResponse();
+            try
+            {
+                //Get the UserId from the Header and update the request object
+                request.UserId = HttpContext.Current.Request.Headers.Get(_phytelUserIDToken);
+                if (string.IsNullOrEmpty(request.UserId))
+                    throw new UnauthorizedAccessException("ProgramDD:Get()::Unauthorized Access");
+
+                response = ProgramDataManager.GetStepResponse(request);
+                response.Version = request.Version;
+            }
+            catch (Exception ex)
+            {
+                CommonFormatter.FormatExceptionResponse(response, base.Response, ex);
+
+                string aseProcessID = ConfigurationManager.AppSettings.Get("ASEProcessID") ?? "0";
+                Common.Helper.LogException(int.Parse(aseProcessID), ex);
+            }
+            return response;
+        }
+
+        public GetProgramAttributeResponse Get(GetProgramAttributeRequest request)
+        {
+            GetProgramAttributeResponse response = new GetProgramAttributeResponse();
+            try
+            {
+                //Get the UserId from the Header and update the request object
+                request.UserId = HttpContext.Current.Request.Headers.Get(_phytelUserIDToken);
+                if (string.IsNullOrEmpty(request.UserId))
+                    throw new UnauthorizedAccessException("ProgramDD:Get()::Unauthorized Access");
+
+                response = ProgramDataManager.GetProgramAttributes(request);
+                response.Version = request.Version;
+            }
+            catch (Exception ex)
+            {
+                CommonFormatter.FormatExceptionResponse(response, base.Response, ex);
+
+                string aseProcessID = ConfigurationManager.AppSettings.Get("ASEProcessID") ?? "0";
+                Common.Helper.LogException(int.Parse(aseProcessID), ex);
+            }
+            return response;
+        }
+
+        public PutUpdateProgramAttributesResponse Put(PutUpdateProgramAttributesRequest request)
+        {
+            PutUpdateProgramAttributesResponse response = new PutUpdateProgramAttributesResponse();
+            try
+            {
+                //Get the UserId from the Header and update the request object
+                request.UserId = HttpContext.Current.Request.Headers.Get(_phytelUserIDToken);
+                if (string.IsNullOrEmpty(request.UserId))
+                    throw new UnauthorizedAccessException("ProgramDD:Put()::Unauthorized Access");
+
+                response = ProgramDataManager.PutUpdateProgramAttributes(request);
+                response.Version = request.Version;
+            }
+            catch (Exception ex)
+            {
+                CommonFormatter.FormatExceptionResponse(response, base.Response, ex);
+
+                string aseProcessID = ConfigurationManager.AppSettings.Get("ASEProcessID") ?? "0";
+                Common.Helper.LogException(int.Parse(aseProcessID), ex);
+            }
+            return response;
+        }
+
+        public PutProgramAttributesResponse Put(PutProgramAttributesRequest request)
+        {
+            PutProgramAttributesResponse response = new PutProgramAttributesResponse();
+            try
+            {
+                //Get the UserId from the Header and update the request object
+                request.UserId = HttpContext.Current.Request.Headers.Get(_phytelUserIDToken);
+                if (string.IsNullOrEmpty(request.UserId))
+                    throw new UnauthorizedAccessException("ProgramDD:Put()::Unauthorized Access");
+
+                response = ProgramDataManager.InsertProgramAttributes(request);
+                response.Version = request.Version;
+            }
+            catch (Exception ex)
+            {
+                CommonFormatter.FormatExceptionResponse(response, base.Response, ex);
+
+                string aseProcessID = ConfigurationManager.AppSettings.Get("ASEProcessID") ?? "0";
+                Common.Helper.LogException(int.Parse(aseProcessID), ex);
+            }
+            return response;
+        }
+    }
+}
