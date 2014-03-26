8d6b5004fc5cd80561f6b3f85e9a74b3f79824c6
Tony DiGiorgio <tony.digiorgio@phytel.com>
2014-03-26 04:38:45 +0000

    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Services;C2065

---
+++ API/AppDomain/Phytel.API.AppDomain.Security/Production/Current/Security.DTOs/AuthenticateRequest.cs
@@ -0,0 +1,22 @@
+﻿using Phytel.API.Interface;
+using ServiceStack.ServiceHost;
+
+namespace Phytel.API.AppDomain.Security.DTO
+{
+    [Api(Description="Request posted when a product needs to authenticate itself to the API and receive a usage token")]
+    [Route("/{Version}/login", "POST")]
+    public class AuthenticateRequest //we don't inherit from IAppDomainRequest in the Security as that interface does not apply
+    {
+        [ApiMember(Name = "Token", Description = "One-Time Token given by the User Application Login process permitting access to the API", ParameterType = "body", DataType = "string", IsRequired = true)]
+        public string Token { get; set; }
+
+        [ApiMember(Name = "APIKey", Description = "API License Key unique to the product requesting access", ParameterType = "body", DataType = "string", IsRequired = true)]
+        public string APIKey { get; set; }
+
+        [ApiMember(Name = "Context", Description = "Context requesting access to the API", ParameterType = "body", DataType = "string", IsRequired = true)]
+        public string Context { get; set; }
+
+        [ApiMember(Name = "Version", Description = "Version of the API being called", ParameterType = "path", DataType = "double", IsRequired = true)]
+        public double Version { get; set; }
+    }
+}
---
+++ API/AppDomain/Phytel.API.AppDomain.Security/Production/Current/Security.Services/Global.asax.cs
@@ -0,0 +1,109 @@
+﻿using ServiceStack.MiniProfiler;
+using ServiceStack.ServiceInterface.Admin;
+using ServiceStack.ServiceInterface.Cors;
+using ServiceStack.WebHost.Endpoints;
+using System;
+using ServiceStack;
+using ServiceStack.WebHost.Endpoints.Support;
+using ServiceStack.ServiceHost;
+using System.Web;
+using ServiceStack.WebHost.Endpoints.Extensions;
+using ServiceStack.Common.Web;
+
+namespace Phytel.API.AppDomain.Security.Service
+{
+    public class Global : System.Web.HttpApplication
+    {
+        public class SecurityAppHost : AppHostBase
+        {
+            //Tell Service Stack the name of your application and where to find your web services
+            public SecurityAppHost() : base("Phytel Security Services", typeof(SecurityService).Assembly) { }
+
+            public override void Configure(Funq.Container container)
+            {
+                //register any dependencies your services use, e.g:
+                //container.Register<ICacheClient>(new MemoryCacheClient());
+                Plugins.Add(new RequestLogsFeature() { RequiredRoles = new string[] { } });
+                
+                var emitGlobalHeadersHandler = new CustomActionHandler((httpReq, httpRes) => httpRes.EndRequest());
+                SetConfig(new EndpointHostConfig
+                {
+                    RawHttpHandlers = { (httpReq) => httpReq.HttpMethod == HttpMethods.Options ? emitGlobalHeadersHandler : null },
+                    GlobalResponseHeaders = { 
+                    //{"Access-Control-Allow-Origin", "*"},
+                    { "Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS" }, 
+                    { "Access-Control-Allow-Headers", "Content-Type" }, },
+                    AllowJsonpRequests = true
+                });
+            }
+        }
+
+        public class CustomActionHandler : IServiceStackHttpHandler, IHttpHandler
+        {
+            public Action<IHttpRequest, IHttpResponse> Action { get; set; }
+
+            public CustomActionHandler(Action<IHttpRequest, IHttpResponse> action)
+            {
+                if (action == null)
+                    throw new Exception("Action was not supplied to ActionHandler");
+
+                Action = action;
+            }
+
+            public void ProcessRequest(IHttpRequest httpReq, IHttpResponse httpRes, string operationName)
+            {
+                Action(httpReq, httpRes);
+            }
+
+            public void ProcessRequest(HttpContext context)
+            {
+                ProcessRequest(context.Request.ToRequest(GetType().Name),
+                    context.Response.ToResponse(),
+                    GetType().Name);
+            }
+
+            public bool IsReusable
+            {
+                get { return false; }
+            }
+        }
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            new SecurityAppHost().Init();
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+        }
+
+        protected void Application_EndRequest(object sender, EventArgs e)
+        {
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
