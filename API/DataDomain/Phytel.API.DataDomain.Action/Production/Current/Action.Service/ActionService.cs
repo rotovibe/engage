a181ba21dbe86db41ebc26ae6dc7a66fd37d78f5
Tony DiGiorgio <tony.digiorgio@phytel.com>
2014-03-26 04:38:56 +0000

    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Services;C2071

---
+++ API/DataDomain/Phytel.API.DataDomain.Action/Production/Current/Action.Service/ActionService.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Net;
+using Phytel.API.DataDomain.Action;
+using Phytel.API.DataDomain.Action.DTO;
+using ServiceStack.ServiceClient.Web;
+using Phytel.API.Common.Format;
+using System.Configuration;
+using System.Web;
+
+namespace Phytel.API.DataDomain.Action.Service
+{
+    public class ActionService : ServiceStack.ServiceInterface.Service
+    {
+        private const string _phytelUserIDToken = "x-Phytel-UserID";
+
+        public GetActionDataResponse Get(GetActionDataRequest request)
+        {
+            GetActionDataResponse response = new GetActionDataResponse();
+            try
+            {
+                //Get the UserId from the Header and update the request object
+                request.UserId = HttpContext.Current.Request.Headers.Get(_phytelUserIDToken);
+                if (string.IsNullOrEmpty(request.UserId))
+                    throw new UnauthorizedAccessException("ActionDD:Get():Unauthorized Access");
+
+                response = ActionDataManager.GetActionByID(request);
+                response.Version = request.Version;
+            }
+            catch (Exception ex)
+            {
+                CommonFormatter.FormatExceptionResponse(response, base.Response, ex);
+
+                string aseProcessID = ConfigurationManager.AppSettings.Get("ASEProcessID") ?? "0";
+                Common.Helper.LogException(int.Parse(aseProcessID), ex);
+            }
+            return response;
+        }
+    }
+}
---
+++ API/DataDomain/Phytel.API.DataDomain.Action/Production/Current/Action/ActionDataManager.cs
@@ -0,0 +1,20 @@
+using Phytel.API.DataDomain.Action.DTO;
+using System.Data.SqlClient;
+using Phytel.API.DataDomain.Action;
+
+namespace Phytel.API.DataDomain.Action
+{
+    public static class ActionDataManager
+    {
+        public static GetActionDataResponse GetActionByID(GetActionDataRequest request)
+        {
+            GetActionDataResponse result = new GetActionDataResponse();
+
+            IActionRepository<GetActionDataResponse> repo = ActionRepositoryFactory<GetActionDataResponse>.GetActionRepository(request.ContractNumber, request.Context, request.UserId);
+
+            result = repo.FindByID(request.ActionID) as GetActionDataResponse;
+            
+            return (result != null ? result : new GetActionDataResponse());
+        }
+    }
+}   
