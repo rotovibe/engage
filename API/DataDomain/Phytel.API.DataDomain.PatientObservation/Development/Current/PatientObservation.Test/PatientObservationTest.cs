42b94f5855516d420efb06762415c0d729b8ecd8
Snehal Ingavale <Snehal.Ingavale@phytel.com>
2014-04-04 16:17:15 +0000

    Added user stories for GetAllowedObservationStates method - Night-871
    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Services;C2167

--- API/DataDomain/Phytel.API.DataDomain.PatientObservation/Development/Current/PatientObservation.Test/PatientObservationTest.cs
+++ API/DataDomain/Phytel.API.DataDomain.PatientObservation/Development/Current/PatientObservation.Test/PatientObservationTest.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Phytel.API.DataDomain.PatientObservation.DTO;
 
@@ -14,6 +15,41 @@ namespace Phytel.API.DataDomain.PatientObservation.Test
             GetPatientObservationResponse response = PatientObservationDataManager.GetPatientObservationByID(request);
 
             Assert.IsTrue(response.PatientObservation.PatientObservationID == "Tony");
+        }
+
+
+        [TestMethod]
+        public void GetAllowedObservationStates_CorrectType_Test()
+        {
+            // Arrange
+            string userId = string.Empty;
+            string contractNumber = "InHealth001";
+            string context = "NG";
+            string type = "Lab";
+
+            IPatientObservationRepository<object> repo =
+                PatientObservationRepositoryFactory<object>.GetPatientObservationRepository(contractNumber, context, userId);
+
+            List<int> types = repo.GetAllowedObservationStates(type);
+            
+            Assert.IsTrue(types.Count > 0);
+        }
+
+        [TestMethod]
+        public void GetAllowedObservationStates_IncorrectType_NullResult_Test()
+        {
+            // Arrange
+            string userId = string.Empty;
+            string contractNumber = "InHealth001";
+            string context = "NG";
+            string type = "Labs";
+
+            IPatientObservationRepository<object> repo =
+                PatientObservationRepositoryFactory<object>.GetPatientObservationRepository(contractNumber, context, userId);
+
+            List<int> types = repo.GetAllowedObservationStates(type);
+
+            Assert.IsNull(types);
         }
     }
 }
