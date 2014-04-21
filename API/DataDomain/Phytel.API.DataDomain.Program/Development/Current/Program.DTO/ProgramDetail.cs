7d0da282c63668cdf91eec348cd654b9017a28d5
Mel Bobadilla <mel.bobadilla@phytel.com>
2014-04-21 21:23:29 +0000

    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Services;C2394

--- API/DataDomain/Phytel.API.DataDomain.Program/Development/Current/Program.DTO/ProgramDetail.cs
+++ API/DataDomain/Phytel.API.DataDomain.Program/Development/Current/Program.DTO/ProgramDetail.cs
@@ -38,5 +38,7 @@ namespace Phytel.API.DataDomain.Program.DTO
         public string DidNotEnrollReason { get; set; }
         public string DisEnrollReason { get; set; }
         public string OverrideReason { get; set; }
+
+        public PatientProgramAttribute Attributes { get; set; }
     }
 }
