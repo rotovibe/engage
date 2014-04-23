834fd8b26c3532dcc5711cb5eb82f5e3abe48dbd
Tony DiGiorgio <tony.digiorgio@phytel.com>
2014-04-23 22:24:33 +0000

    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Services;C2454

--- API/Audit/DataAudit/Development/Current/DataAuditProcessor/DataAuditFailureProcessor.cs
+++ API/Audit/DataAudit/Development/Current/DataAuditProcessor/DataAuditFailureProcessor.cs
@@ -1,15 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml;
-using Phytel.Framework.ASE.Process;
-using Phytel.API.DataAudit;
-using Phytel.Mongo.Linq;
-using System.Configuration;
-using MongoDB.Driver;
 using System.IO;
+using Phytel.API.DataDomain.ASE.Common.Enums;
+using Phytel.API.DataDomain.ASE.DTO.Message;
+using Phytel.ASE.Core;
 
 namespace Phytel.API.DataAuditProcessor
 {
@@ -29,31 +22,8 @@ namespace Phytel.API.DataAuditProcessor
             }
             catch(Exception ex)
             {
-                base.LogError(ex, Framework.ASE.Data.Common.LogErrorCode.Error, Framework.ASE.Data.Common.LogErrorSeverity.Critical);
+                base.LogError(ex, LogErrorCode.Error, LogErrorSeverity.Critical);
             }
         }
-
-        //private void SetupBaseProperties()
-        //{
-        //    _message = _bodyDom.SelectSingleNode(string.Format(_xpath, "Message"));
-        //    _entityid = GetDomValue("EntityID");
-        //    _userid = GetDomValue("UserId");
-        //    _contractnumber = GetDomValue("Contract");
-        //    _entitytype = GetDomValue("EntityType");
-        //    _type = GetDomValue("Type");
-        //    _timestamp = DateTime.Parse(GetDomValue("TimeStamp").ToString());
-        //    _entity = GetDomValue("Entity");
-        //}
-
-        //private string GetDomValue(string fieldName)
-        //{
-        //    XmlNode xnode = _bodyDom.SelectSingleNode(string.Format(_xpath, fieldName));
-
-        //    if (xnode != null)
-        //        return xnode.InnerText;
-        //    else
-        //        return string.Empty;
-        //}
-
     }
 }
