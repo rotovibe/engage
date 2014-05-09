128d9e1b7e2c771b6770dfb9c226a204557583e3
Tony DiGiorgio <tony.digiorgio@phytel.com>
2014-05-09 12:13:11 +0000

    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Services;C2738

---
+++ API/DataDomain/Phytel.API.DataDomain.PatientProblem/Production/0.1.0.0/PatientProblem.DTOs/GetAllPatientProblemsDataRequest.cs
@@ -0,0 +1,15 @@
+using Phytel.API.Interface;
+using ServiceStack.ServiceHost;
+
+namespace Phytel.API.DataDomain.PatientProblem.DTO
+{
+    [Route("/{Context}/{Version}/{ContractNumber}/Patient/{PatientID}/Problems", "GET")]
+    public class GetAllPatientProblemsDataRequest: IDataDomainRequest
+    {
+        public string PatientID { get; set; }
+        public string Context { get; set; }
+        public string ContractNumber { get; set; }
+        public double Version { get; set; }
+        public string UserId { get; set; }
+    }
+}
