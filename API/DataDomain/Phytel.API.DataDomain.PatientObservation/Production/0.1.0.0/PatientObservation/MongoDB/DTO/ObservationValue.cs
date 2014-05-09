a022e0bd3a4a17691a161e9f5babf164261bbadd
Tony DiGiorgio <tony.digiorgio@phytel.com>
2014-05-09 12:13:29 +0000

    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Services;C2740

---
+++ API/DataDomain/Phytel.API.DataDomain.PatientObservation/Production/0.1.0.0/PatientObservation/MongoDB/DTO/ObservationValue.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Phytel.API.DataDomain.PatientObservation.MongoDB.DTO
+{
+    public class ObservationValue
+    {
+        public ObservationValue() { }
+
+        public const string ValueIdProperty = "vid";
+        [BsonElement(ValueIdProperty)]
+        [BsonIgnoreIfNull(true)]
+        public ObjectId ValueId { get; set; }
+
+        public const string ValueProperty = "val";
+        [BsonElement(ValueProperty)]
+        [BsonIgnoreIfNull(true)]
+        public string Value { get; set; }
+
+        public const string TextProperty = "txt";
+        [BsonElement(TextProperty)]
+        [BsonIgnoreIfNull(true)]
+        public string Text { get; set; }
+    }
+}
