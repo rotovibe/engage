15f99bcfb1e5d9fb967f18581e0be18ce1358eaa
Tony DiGiorgio <tony.digiorgio@phytel.com>
2014-05-09 12:20:19 +0000

    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Services;C2763

---
+++ API/Common/Phytel.API.Common/Production/0.1.0.0/CustomObject/IdNamePair.cs
@@ -0,0 +1,16 @@
+﻿namespace Phytel.API.Common.CustomObject
+{
+    public class IdNamePair
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+
+        public IdNamePair() {}
+
+        public IdNamePair(string id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
