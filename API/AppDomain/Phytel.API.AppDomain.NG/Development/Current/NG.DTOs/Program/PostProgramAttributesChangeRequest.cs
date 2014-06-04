cb67ee499326f488c26e94d4b3a5330636d9b5d5
Mel Bobadilla <mel.bobadilla@phytel.com>
2014-06-04 16:52:36 +0000

    changed request metadata
    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Services;C3470

--- API/AppDomain/Phytel.API.AppDomain.NG/Development/Current/NG.DTOs/Program/PostProgramAttributesChangeRequest.cs
+++ API/AppDomain/Phytel.API.AppDomain.NG/Development/Current/NG.DTOs/Program/PostProgramAttributesChangeRequest.cs
@@ -12,7 +12,7 @@ namespace Phytel.API.AppDomain.NG.DTO
         [ApiMember(Name = "ProgramId", Description = "ProgramId", ParameterType = "path", DataType = "String", IsRequired = true)]
         public string ProgramId { get; set; }
 
-        [ApiMember(Name = "Properties", Description = "Plan Element graph with properties to change", ParameterType = "body", DataType = "string", IsRequired = true)]
+        [ApiMember(Name = "Properties", Description = "Plan Element graph with properties to change", ParameterType = "body", DataType = "PlanElementProperties", IsRequired = true)]
         public PlanElementProperties Properties { get; set; }
 
         [ApiMember(Name = "PatientId", Description = "PatientId", ParameterType = "path", DataType = "string", IsRequired = true)]
