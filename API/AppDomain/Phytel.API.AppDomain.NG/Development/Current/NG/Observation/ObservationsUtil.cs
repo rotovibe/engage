b0c3cb8566e174733c3275fad5efc4371c35b838
Snehal Ingavale <Snehal.Ingavale@phytel.com>
2014-08-11 19:24:25 +0000

    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Services;C5128

--- API/AppDomain/Phytel.API.AppDomain.NG/Development/Current/NG/Observation/ObservationsUtil.cs
+++ API/AppDomain/Phytel.API.AppDomain.NG/Development/Current/NG/Observation/ObservationsUtil.cs
@@ -305,7 +305,8 @@ namespace Phytel.API.AppDomain.NG
                         GroupId = po.GroupId,
                         Name = po.Name,
                         Standard = po.Standard,
-                        TypeId = po.TypeId
+                        TypeId = po.TypeId,
+                        DeleteFlag = po.DeleteFlag
                     };
                 }
             }
