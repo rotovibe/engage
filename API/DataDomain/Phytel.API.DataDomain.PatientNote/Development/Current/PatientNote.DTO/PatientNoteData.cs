00512a5a6f571e7663945fecc4d000973e811e96
Snehal Ingavale <Snehal.Ingavale@phytel.com>
2014-09-09 21:28:49 +0000

    Adding additional attributes to patientnotes for Touchpoints
    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Services;C5730

--- API/DataDomain/Phytel.API.DataDomain.PatientNote/Development/Current/PatientNote.DTO/PatientNoteData.cs
+++ API/DataDomain/Phytel.API.DataDomain.PatientNote/Development/Current/PatientNote.DTO/PatientNoteData.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Phytel.API.AppDomain.NG.DTO
+namespace Phytel.API.DataDomain.PatientNote.DTO
 {
-    public class PatientNote
+    public class PatientNoteData
     {
         public string Id { get; set; }
         public string PatientId { get; set; }
