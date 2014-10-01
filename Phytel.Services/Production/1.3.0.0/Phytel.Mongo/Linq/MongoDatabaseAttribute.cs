891b722ef719c86e4d339aa35ab4d7e30c236dc2
Tony DiGiorgio <tony.digiorgio@phytel.com>
2014-10-01 11:03:01 +0000

    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Shared;C6605

---
+++ Phytel.Services/Production/1.3.0.0/Phytel.Mongo/Linq/MongoDatabaseAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Phytel.Mongo.Linq
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple=false, Inherited=false)]
+    public class MongoDatabaseAttribute : Attribute
+    {
+        public string DatabaseName { get; set; }
+    }
+}
