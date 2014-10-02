c0ef2d82146aa40581490c06ff699569daba5317
Tony DiGiorgio <tony.digiorgio@phytel.com>
2014-10-02 18:22:49 +0000

    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Shared;C6754

--- Phytel.Services/Nuget/Phytel.Services.Web/PreRelease/Properties/AssemblyInfo.cs
+++ Phytel.Services/Nuget/Phytel.Services.Web/PreRelease/Properties/AssemblyInfo.cs
@@ -32,3 +32,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 [assembly: AssemblyVersion("1.3.0.1")]
 [assembly: AssemblyFileVersion("1.3.0.1")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
