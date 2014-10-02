846a39966a83d4971454e72ca6aa9c09a309aee0
Tony DiGiorgio <tony.digiorgio@phytel.com>
2014-10-02 20:23:55 +0000

    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Shared;C6767

---
+++ Phytel.Services/Nuget/Phytel.Services.Mongo/Stable/Linq/IMongoEntity.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Phytel.Services.Mongo.Linq
+{
+    /// <summary>
+    /// Interface that all MongoSet<typeparamref name="T"/> types must implement
+    /// </summary>
+    public interface IMongoEntity<TKey>
+    {
+        TKey Id { get; }
+    }
+}
