8dd5a1e5b68c96fc82f0b44fd99c5387a0ee9aff
Snehal Ingavale <Snehal.Ingavale@phytel.com>
2014-10-21 20:59:07 +0000

    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Services;C7273

--- API/AppDomain/Phytel.API.AppDomain.NG/Development/Current/AppDomain.NG.Tests/Goal_Tests.cs
+++ API/AppDomain/Phytel.API.AppDomain.NG/Development/Current/AppDomain.NG.Tests/Goal_Tests.cs
@@ -33,8 +33,8 @@ namespace Phytel.API.AppDomain.NG.Test
             request.ContractNumber = "InHealth001";
             request.UserId = "AD_TestHarness";
             request.Version = 1;
-            request.Id = "5303a5ccd4332316b4a69449";
-            request.PatientId = "52f55877072ef709f84e69b0";
+            request.Id = "543c2eb284ac0509803de12a";
+            request.PatientId = "543c2e6b84ac050980224c50";
             request.UserId = "Snehal";
 
             GoalsManager gManager = new GoalsManager();
@@ -50,7 +50,7 @@ namespace Phytel.API.AppDomain.NG.Test
             request.ContractNumber = "InHealth001";
             request.UserId = "531f2df5072ef727c4d2a3bc";
             request.Version = 1;
-            request.PatientId = "5325da76d6a4850adcbba656";
+            request.PatientId = "543c2e6b84ac050980224c50";
 
             GoalsManager gManager = new GoalsManager();
             GetAllPatientGoalsResponse response = gManager.GetAllPatientGoals(request);
@@ -100,8 +100,8 @@ namespace Phytel.API.AppDomain.NG.Test
             GetTasksRequest request = new GetTasksRequest();
             request.ContractNumber = "InHealth001";
             request.Version = 1;
-            request.StatusIds = new List<int> { 1, 2 };
-           // request.PatientId = "5325db97d6a4850adcbba9ba";
+            request.StatusIds = new List<int> { 1, 2, 3 };
+            request.PatientId = "543c2e6b84ac050980224c50";
             request.UserId = "531f2df5072ef727c4d2a3bc";
 
             GoalsManager gManager = new GoalsManager();
@@ -143,22 +143,22 @@ namespace Phytel.API.AppDomain.NG.Test
             request.ContractNumber = "InHealth001";
             request.Version = 1;
             request.UserId = "531f2df5072ef727c4d2a3bc";
-            request.Id = "53308ee8d6a4850998d7fc69";
+            request.Id = "54403a1e84ac05070c5685a2";
             request.Intervention = new PatientIntervention
             {
-                Id = "53308ee8d6a4850998d7fc69",
+                Id = "54403a1e84ac05070c5685a2",
                 AssignedToId = "5325c821072ef705080d3488",
-                BarrierIds = new List<string> { "53308ee0d6a4850998d7fc63" },
-                CategoryId = "52fa624ad433231dd077501f",
-                Description = "testing save",
-                PatientGoalId = "53308e94d6a4850998d7fc5f",
+                //BarrierIds = new List<string> { "53308ee0d6a4850998d7fc63" },
+                //CategoryId = "52fa624ad433231dd077501f",
+                Description = "int2",
+                PatientGoalId = "5440392884ac05070c5682ae",
                 StartDate = DateTime.UtcNow,
                 StatusDate = DateTime.UtcNow,
-                StatusId = 2,
-                ClosedDate = DateTime.UtcNow
+                StatusId = 1,
+                ClosedDate = DateTime.Parse("2014-10-16T21:50:47.057Z")
             };
-            request.PatientGoalId = "53308e94d6a4850998d7fc5f";
-            request.PatientId = "5325da9ad6a4850adcbba6c2";
+            request.PatientGoalId = "5440392884ac05070c5682ae";
+            request.PatientId = "5325dae6d6a4850adcbba7ae";
 
             GoalsManager gManager = new GoalsManager();
             PostPatientInterventionResponse response = gManager.SavePatientIntervention(request);
@@ -174,14 +174,14 @@ namespace Phytel.API.AppDomain.NG.Test
             request.ContractNumber = "InHealth001";
             request.Version = 1;
             request.UserId = "531f2df5072ef727c4d2a3bc";
-            request.Id = "53308ee5d6a4850998d7fc67";
+            request.Id = "543c2f3f84ac0509803de5d2";
             request.Task = new PatientTask
             {
-                Id = "53308ee5d6a4850998d7fc67",
+                Id = "543c2f3f84ac0509803de5d2",
                // BarrierIds = new List<string> { "53308ee3d6a4850998d7fc65" },
               // CustomAttributes = new List<CustomAttribute> { };
-                Description = "task 2",
-                PatientGoalId = "53308e94d6a4850998d7fc5f",
+                Description = "task 3",
+                PatientGoalId = "543c2eb284ac0509803de12a",
                 StartDate = DateTime.UtcNow,
                 StatusDate = DateTime.UtcNow,
                 StatusId = 3,
@@ -189,8 +189,8 @@ namespace Phytel.API.AppDomain.NG.Test
                 TargetDate = DateTime.UtcNow,
                 TargetValue = "new value"
             };
-            request.PatientGoalId = "53308e94d6a4850998d7fc5f";
-            request.PatientId = "5325da9ad6a4850adcbba6c2";
+            request.PatientGoalId = "543c2eb284ac0509803de12a";
+            request.PatientId = "543c2e6b84ac050980224c50";
 
             GoalsManager gManager = new GoalsManager();
             PostPatientTaskResponse response = gManager.SavePatientTask(request);
