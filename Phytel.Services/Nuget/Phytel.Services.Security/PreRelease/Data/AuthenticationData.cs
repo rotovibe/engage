32aad541b1142a434dae8a676fa0929b3a2f73f4
Joe Rupert <Joe.Rupert@phytel.com>
2014-10-22 18:58:52 +0000

    Security service work to add 1 way hashing and an authentication manager (with abstraction) to handle new key/salt generation and validation
    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Shared;C7290

---
+++ Phytel.Services/Nuget/Phytel.Services.Security/PreRelease/Data/AuthenticationData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Phytel.Services.Security
+{
+    public class AuthenticationData
+    {
+        //public AuthenticationData(byte[] key, byte[] salt)
+        //    : this(key, salt, string.Empty)
+        //{ }
+
+        //public AuthenticationData(string key, string salt)
+        //    : this(key, salt, string.Empty)
+        //{ }
+
+        public AuthenticationData(byte[] key, byte[] salt, string passphrase)
+            : this(Convert.ToBase64String(key), Convert.ToBase64String(salt), passphrase)
+        { }
+
+        public AuthenticationData(string key, string salt, string passphrase)
+        {
+            EncodedKey = key;
+            EncodedSalt = salt;
+            Passphrase = passphrase;
+        }
+
+        public string EncodedKey { get; private set; }
+
+        public string EncodedSalt { get; private set; }
+
+        public string Passphrase { get; set; }
+    }
+}
---
+++ Phytel.Services/Nuget/Phytel.Services.Security/PreRelease/Phyel.Services.Security.Test/AuthenticationManagerTests/AuthenticationManagerSHA1Tests.cs
@@ -0,0 +1,75 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Phytel.Services.Security.Text
+{
+    [TestFixture]
+    class AuthenticationManagerSHA1Tests
+    {
+        protected IAuthenticationManager _authenticationManager;
+
+        [TestFixtureSetUp]
+        public void Initialize()
+        {
+            _authenticationManager = new AuthenticationManagerSHA1();
+        }
+
+        [TestCase]
+        public void GenerateAuthenticationDataFromPassphrase_ReturnData_NotNull()
+        {
+            string passphrase = "TestPassphrase";
+            AuthenticationData data = _authenticationManager.GenerateAuthenticationDataFromPassphrase(passphrase);
+            Assert.IsNotNull(data);
+            Assert.IsNotNull(data.EncodedKey);
+            Assert.IsNotNull(data.EncodedSalt);
+            Assert.AreEqual(passphrase, data.Passphrase);
+        }
+
+        [TestCase]
+        public void GenerateAuthenticationDataFromPassphrase_ReturnData_PassphraseAreEqual()
+        {
+            string passphrase = "TestPassphrase";
+            AuthenticationData data = _authenticationManager.GenerateAuthenticationDataFromPassphrase(passphrase);
+            Assert.IsNotNull(data);
+            Assert.AreEqual(passphrase, data.Passphrase);
+        }
+
+        [TestCase]
+        public void PassphraseIsValid_AttemptValidation_Success()
+        {
+            string passphrase = "TestPassphrase";
+            AuthenticationData data = _authenticationManager.GenerateAuthenticationDataFromPassphrase(passphrase);
+
+            Assert.IsTrue(_authenticationManager.PassphraseIsValid(data.EncodedKey, data.EncodedSalt, passphrase));
+        }
+
+        [TestCase]
+        public void PassphraseIsValid_AttemptValidation_Failure()
+        {
+            string passphrase = "TestPassphrase";
+            string incorrectPassphrase = "FAIL";
+            AuthenticationData data = _authenticationManager.GenerateAuthenticationDataFromPassphrase(passphrase);
+
+            Assert.IsFalse(_authenticationManager.PassphraseIsValid(data.EncodedKey, data.EncodedSalt, incorrectPassphrase));
+        }
+
+        [TestCase]
+        public void PassphraseIsValid_AttemptValidationOverload_Success()
+        {
+            string passphrase = "TestPassphrase";
+            AuthenticationData data = _authenticationManager.GenerateAuthenticationDataFromPassphrase(passphrase);
+
+            Assert.IsTrue(_authenticationManager.PassphraseIsValid(Convert.FromBase64String(data.EncodedKey), Convert.FromBase64String(data.EncodedSalt), passphrase));
+        }
+
+        [TestCase]
+        public void PassphraseIsValid_AttemptValidationOverload_Failure()
+        {
+            string passphrase = "TestPassphrase";
+            string incorrectPassphrase = "FAIL";
+            AuthenticationData data = _authenticationManager.GenerateAuthenticationDataFromPassphrase(passphrase);
+
+            Assert.IsFalse(_authenticationManager.PassphraseIsValid(Convert.FromBase64String(data.EncodedKey), Convert.FromBase64String(data.EncodedSalt), incorrectPassphrase));
+        }
+    }
+}
