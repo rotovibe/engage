23210b915f94255f864611d86a6cae8a3e6edc76
Snehal Ingavale <Snehal.Ingavale@phytel.com>
2014-10-27 22:06:47 +0000

    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Services;C7553

--- API/DataDomain/Phytel.API.DataDomain.Allergy/Development/Current/Allergy/PatientAllergyDataManager.cs
+++ API/DataDomain/Phytel.API.DataDomain.Allergy/Development/Current/Allergy/PatientAllergyDataManager.cs
@@ -21,6 +21,7 @@ namespace Phytel.API.DataDomain.Allergy
             {
                 List<PatientAllergyData> result = null;
                 AllergyRepository.UserId = request.UserId;
+                // Mel, I commented out line below, since it was failing. Fix this when you reach here.
                // result = AllergyRepository.SelectAll().Cast<PatientAllergyData>().ToList<PatientAllergyData>();
 
                 return result;
