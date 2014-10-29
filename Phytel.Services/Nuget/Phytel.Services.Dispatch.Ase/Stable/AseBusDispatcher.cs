098f065bab7efb2cca9b0fb10df84caa35bde90e
Charles Zipp <charles.zipp@phytel.com>
2014-10-29 18:36:11 +0000

    Branched from $/PhytelCode/Phytel.Net/Shared/Phytel.Services/Nuget/Phytel.Services.Dispatch.Ase/PreRelease
    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Shared;C7617

---
+++ Phytel.Services/Nuget/Phytel.Services.Dispatch.Ase/Stable/AseBusDispatcher.cs
@@ -0,0 +1,34 @@
+﻿using Phytel.Bus;
+using Phytel.Framework.ASE.Bus.Data;
+using Phytel.Services.Serializer;
+using System;
+using System.Collections.Generic;
+
+namespace Phytel.Services.Dispatch.Ase
+{
+    public class AseBusDispatcher : Dispatcher
+    {
+        protected readonly MessageBus _messageBus;
+        protected readonly string _publishKey;
+
+        public AseBusDispatcher(string publishKey, ISerializer serializer)
+            : this(new MessageBus(), publishKey, serializer)
+        {
+        }
+
+        public AseBusDispatcher(MessageBus messageBus, string publishKey, ISerializer serializer)
+            : base(serializer)
+        {
+            _messageBus = messageBus;
+            _publishKey = publishKey;
+        }
+
+        protected override void OnDispatch(string message)
+        {
+            PublishMessage publishMessage = new PublishMessage();
+            publishMessage.Message = message;
+            publishMessage.PublishKey = _publishKey;
+            Tuple<List<SubscriberQueue>, List<PublishResult>> results = _messageBus.PutOnBus(publishMessage);
+        }
+    }
+}
