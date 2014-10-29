76e6f0c25829047e79fc5d93875db2d70d1370af
Charles Zipp <charles.zipp@phytel.com>
2014-10-29 18:42:08 +0000

    Branched from $/PhytelCode/Phytel.Net/Shared/Phytel.Services/Nuget/Phytel.Services.AppSettings/PreRelease
    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Shared;C7618

---
+++ Phytel.Services/Nuget/Phytel.Services.AppSettings/Stable/AppSettingsProvider.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Specialized;
+using System.Configuration;
+
+namespace Phytel.Services.AppSettings
+{
+    public class AppSettingsProvider : IAppSettingsProvider
+    {
+        protected readonly NameValueCollection _appSettings;
+
+        public AppSettingsProvider()
+            : this(ConfigurationManager.AppSettings)
+        {
+        }
+
+        public AppSettingsProvider(NameValueCollection appSettings)
+        {
+            _appSettings = appSettings;
+        }
+
+        public string Get(string key)
+        {
+            string rvalue = string.Empty;
+
+            if (_appSettings.HasKeys())
+            {
+                rvalue = _appSettings[key];
+            }
+
+            return rvalue;
+        }
+    }
+}
