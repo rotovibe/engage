89f6758dd84af228b11a080dc465e51b472638f7
Charles Zipp <charles.zipp@phytel.com>
2014-10-30 21:01:03 +0000

    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Shared;C7677

---
+++ Phytel.Services/Nuget/Phytel.Services.Dates/PreRelease/IDateTimeProxy.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Phytel.Services.Dates
+{
+    public interface IDateTimeProxy
+    {
+        DateTime GetCurrentDate();
+
+        DateTime GetCurrentDateTime();
+
+        DateTime GetCurrentDateTimeAdd(double minutes);
+
+        TimeSpan GetTimeSpanFromMinutes(double minutes);
+
+        string GetTimestamp();
+
+        string GetTimestampDate();
+
+        string GetTimestampTime();
+    }
+}
