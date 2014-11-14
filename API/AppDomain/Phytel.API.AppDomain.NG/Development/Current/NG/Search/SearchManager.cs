4b1639464adf989fcb58bc3cca78dd7de707deef
Snehal Ingavale <Snehal.Ingavale@phytel.com>
2014-11-14 20:02:29 +0000

    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Services;C8137

--- API/AppDomain/Phytel.API.AppDomain.NG/Development/Current/NG/Search/SearchManager.cs
+++ API/AppDomain/Phytel.API.AppDomain.NG/Development/Current/NG/Search/SearchManager.cs
@@ -145,7 +145,7 @@ namespace Phytel.API.AppDomain.NG.Allergy
                         var pair = new TextValuePair
                         {
                             Text = m.Strength + " " + m.Unit,
-                            Value = m.Strength
+                            Value = m.Strength + " " + m.Unit
                         };
 
                         var rec = vals.Find(f => f.Text == m.Strength + " " + m.Unit);
