e0a88cf21ab6def1c8373ada938854844ae3db40
Biju Joseph <biju.joseph@phytel.com>
2014-12-11 22:38:39 +0000

    Added Test projects
    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Services;C9016

---
+++ API/DataDomain/Phytel.API.DataDomain.Contract/Development/Current/DataDomain.Contract.Test/Contract_Tests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataDomain.Contract.Test
+{
+    [TestClass]
+    public class Contract_Tests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
