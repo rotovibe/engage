b359ddc0d144f8f50f94025e106effd4391fb825
Charles Zipp <charles.zipp@phytel.com>
2015-02-11 15:40:05 +0000

    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Shared;C10401

---
+++ Phytel.Services/Nuget/Phytel.Services.API.Provider/PreRelease/Interface/IHostContextProxy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Phytel.Services.API.Provider
+{
+    public interface IHostContextProxy
+    {
+        T GetItem<T>(string key) where T : class;
+
+        object GetItem(string key);
+
+        string GetItemAsString(string key);
+
+        double GetItemAsDouble(string key);
+
+        void SetItem(string key, object value);
+    }
+}
