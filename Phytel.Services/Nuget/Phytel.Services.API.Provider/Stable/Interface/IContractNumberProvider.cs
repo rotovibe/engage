089e4cd502007d8dd1aec4f6cff978c8c55a91e4
Charles Zipp <charles.zipp@phytel.com>
2015-02-11 15:40:43 +0000

    Branched from $/PhytelCode/Phytel.Net/Shared/Phytel.Services/Nuget/Phytel.Services.API.Provider/PreRelease
    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Shared;C10402

---
+++ Phytel.Services/Nuget/Phytel.Services.API.Provider/Stable/Interface/IContractNumberProvider.cs
@@ -0,0 +1,11 @@
+﻿using Phytel.Services.API.DTO;
+
+namespace Phytel.Services.API.Provider
+{
+    public interface IContractNumberProvider
+    {
+        string Get(IContractRequest contractRequest);
+
+        string Get(object requestDto);
+    }
+}
