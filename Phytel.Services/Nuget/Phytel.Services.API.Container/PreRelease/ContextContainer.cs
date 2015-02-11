abc60177a27ca8daa458b0f2dca2a02a593dc079
Charles Zipp <charles.zipp@phytel.com>
2015-02-11 16:13:38 +0000

    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Shared;C10412

--- Phytel.Services/Nuget/Phytel.Services.API.Container/PreRelease/ContextContainer.cs
+++ Phytel.Services/Nuget/Phytel.Services.API.Container/PreRelease/ContextContainer.cs
@@ -1,8 +1,8 @@
-﻿using Phytel.Services.IOC;
-using Phytel.Services.ServiceStack.Proxy;
+﻿using Phytel.Services.API.Provider;
+using Phytel.Services.IOC;
 using ServiceStack.Common;
 
-namespace Phytel.Services.ServiceStack
+namespace Phytel.Services.API.Container
 {
     public class ContextContainer : ContainerDecorator
     {
@@ -16,10 +16,10 @@ namespace Phytel.Services.ServiceStack
             container.Register<HostContext>(c => HostContext.Instance).ReusedWithin(Funq.ReuseScope.Request);
             container.RegisterAutoWiredAs<HostContextProxy, IHostContextProxy>().ReusedWithin(Funq.ReuseScope.Request);
 
-            container.Register<string>(Constants.NamedStringContext, c =>
+            container.Register<string>(API.Container.Constants.NamedStringContext, c =>
             {
                 IHostContextProxy hostContextProxy = c.Resolve<IHostContextProxy>();
-                return hostContextProxy.GetItem<string>(Constants.HostContextKeyContext);
+                return hostContextProxy.GetItem<string>(API.Provider.Constants.HostContextKeyContext);
             }).ReusedWithin(Funq.ReuseScope.Request);
         }
     }
