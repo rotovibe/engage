14a1564d5bee0b0398f0348c1f9cf5a31911fbe9
Charles Zipp <charles.zipp@phytel.com>
2015-02-11 15:29:13 +0000

    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Shared;C10399

---
+++ Phytel.Services/Nuget/Phytel.Services.API.DTO/PreRelease/IDomainResponse.cs
@@ -0,0 +1,9 @@
+﻿using ServiceStack.ServiceInterface.ServiceModel;
+
+namespace Phytel.Services.API.DTO
+{
+    public interface IDomainResponse
+    {
+        ResponseStatus Status { get; set; }
+    }
+}
