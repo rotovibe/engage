e9b4bf34ddac9dcec331d8099256c7ea6b551454
Mel Bobadilla <mel.bobadilla@phytel.com>
2015-02-18 19:55:32 +0000

    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/Nightingale;C10703

---
+++ Reporting/Development/Current/Reporting/EngageReporting/ETL_Scripts/Create_Tables.sql
@@ -0,0 +1,334 @@
+﻿IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[RPT_BSHSI_HW2_Enrollment_Info]') AND type in (N'U'))
+DROP TABLE [dbo].[RPT_BSHSI_HW2_Enrollment_Info]
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[RPT_AllergyTypeLookUp]') AND type in (N'U'))
+DROP TABLE [dbo].[RPT_AllergyTypeLookUp]
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[RPT_AllergySourceLookUp]') AND type in (N'U'))
+DROP TABLE [dbo].[RPT_AllergySourceLookUp]
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[RPT_SeverityLookUp]') AND type in (N'U'))
+DROP TABLE [dbo].[RPT_SeverityLookUp]
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[RPT_ReactionLookUp]') AND type in (N'U'))				
+DROP TABLE [dbo].[RPT_ReactionLookUp]
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[RPT_MedSupTypeLookUp]') AND type in (N'U'))				
+DROP TABLE [dbo].[RPT_MedSupTypeLookUp]
+GO 
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[RPT_FreqHowOftenLookUp]') AND type in (N'U'))				
+DROP TABLE [dbo].[RPT_FreqHowOftenLookUp]
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[RPT_FreqWhenLookUp]') AND type in (N'U'))	
+DROP TABLE [dbo].[RPT_FreqWhenLookUp]
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[RPT_PatientAllergyReaction]') AND type in (N'U'))	
+DROP TABLE [dbo].[RPT_PatientAllergyReaction]
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[RPT_Allergy]') AND type in (N'U'))	
+DROP TABLE [dbo].[RPT_Allergy]
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[RPT_AllergyType]') AND type in (N'U'))	
+DROP TABLE [dbo].[RPT_AllergyType]
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[RPT_PatientMedSupp]') AND type in (N'U'))	
+DROP TABLE [dbo].[RPT_PatientMedSupp]
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[RPT_PatientMedSuppNDC]') AND type in (N'U'))	
+DROP TABLE [dbo].[RPT_PatientMedSuppNDC]
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[RPT_PatientMedSuppPhClass]') AND type in (N'U'))	
+DROP TABLE [dbo].[RPT_PatientMedSuppPhClass]
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[RPT_PatientAllergy]') AND type in (N'U'))	
+DROP TABLE [dbo].[RPT_PatientAllergy]
+GO
+
+CREATE TABLE [dbo].[RPT_BSHSI_HW2_Enrollment_Info](
+	[PatientId] [int] NOT NULL,
+	[PatientProgramId] [int] NOT NULL,
+	[Priority] [varchar](50) NULL,
+	[firstName] [varchar](100) NULL,
+	[SystemId] [varchar](50) NULL,
+	[LastName] [varchar](100) NULL,
+	[MiddleName] [varchar](100) NULL,
+	[Suffix] [varchar](50) NULL,
+	[Gender] [varchar](50) NULL,
+	[DateOfBirth] [varchar](50) NULL,
+	[LSSN] [int] NULL,
+	[Assigned_PCM] [varchar](100) NULL,
+	[Program_CM] [varchar](100) NULL,
+	[Enrollment] [varchar](50) NULL,
+	[GraduatedFlag] [varchar](50) NULL,
+	[StartDate] [datetime] NULL,
+	[EndDate] [datetime] NULL,
+	[Assigned_Date] [datetime] NULL,
+	[Last_State_Update_Date] [datetime] NULL,
+	[State] [varchar](50) NULL,
+	[Eligibility] [varchar](50) NULL,
+	[Program_Completed_Date] [date] NULL,
+	[Re_enrollment_Date] [date] NULL,
+	[Enrolled_Date] [date] NULL,
+	[Pending_Enrolled_Date] [date] NULL,
+	[Enrollment_Action_Completion_Date] [datetime] NULL,
+	[Market] [varchar](max) NULL,
+	[Disenroll_Date] [date] NULL,
+	[Disenroll_Reason] [varchar](max) NULL,
+	[did_not_enroll_date] [date] NULL,
+	[did_not_enroll_reason] [varchar](max) NULL
+) ON [PRIMARY]
+GO
+CREATE TABLE [dbo].[RPT_AllergyTypeLookUp](
+	[AllergyTypeId] [int] IDENTITY(1,1) NOT NULL,
+	[MongoId] [varchar](100) NOT NULL,
+	[lookUpType] [varchar](200) NULL,
+	[Name] [varchar](300) NULL,
+	[CodeSystem] [varchar](100) NULL,
+	[Code] [varchar](100) NULL,
+ CONSTRAINT [PK_RPT_AllergyTypeLookUp] PRIMARY KEY CLUSTERED 
+(
+	[AllergyTypeId] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+CREATE TABLE [dbo].[RPT_AllergySourceLookUp](
+	[AllergySourceId] [int] IDENTITY(1,1) NOT NULL,
+	[MongoId] [varchar](100) NOT NULL,
+	[lookUpType] [varchar](200) NULL,
+	[Name] [varchar](300) NULL,
+	[Active] [varchar](50) NULL,
+	[Default] [varchar](50) NULL,
+ CONSTRAINT [PK_RPT_AllergySourceLookUp] PRIMARY KEY CLUSTERED 
+(
+	[AllergySourceId] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+CREATE TABLE [dbo].[RPT_SeverityLookUp](
+	[SeverityId] [int] IDENTITY(1,1) NOT NULL,
+	[MongoId] [varchar](100) NOT NULL,
+	[lookUpType] [varchar](200) NULL,
+	[Name] [varchar](300) NULL,
+	[CodeSystem] [varchar](100) NULL,
+	[Code] [varchar](100) NULL,
+ CONSTRAINT [PK_RPT_SeverityLookUp] PRIMARY KEY CLUSTERED 
+(
+	[SeverityId] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+CREATE TABLE [dbo].[RPT_ReactionLookUp](
+	[ReactionId] [int] IDENTITY(1,1) NOT NULL,
+	[MongoId] [varchar](100) NOT NULL,
+	[lookUpType] [varchar](200) NULL,
+	[Name] [varchar](300) NULL,
+	[CodeSystem] [varchar](100) NULL,
+	[Code] [varchar](100) NULL,
+ CONSTRAINT [PK_RPT_ReactionLookUp] PRIMARY KEY CLUSTERED 
+(
+	[ReactionId] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+CREATE TABLE [dbo].[RPT_MedSupTypeLookUp](
+	[MedSupId] [int] IDENTITY(1,1) NOT NULL,
+	[MongoId] [varchar](100) NOT NULL,
+	[lookUpType] [varchar](200) NULL,
+	[Name] [varchar](300) NULL
+ CONSTRAINT [PK_RPT_MedSupTypeLookUp] PRIMARY KEY CLUSTERED 
+(
+	[MedSupId] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+CREATE TABLE [dbo].[RPT_FreqHowOftenLookUp](
+	[FreqId] [int] IDENTITY(1,1) NOT NULL,
+	[MongoId] [varchar](100) NOT NULL,
+	[lookUpType] [varchar](200) NULL,
+	[Name] [varchar](300) NULL
+ CONSTRAINT [PK_RPT_FreqHowOftenLookUp] PRIMARY KEY CLUSTERED 
+(
+	[FreqId] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+CREATE TABLE [dbo].[RPT_FreqWhenLookUp](
+	[FreqWhenId] [int] IDENTITY(1,1) NOT NULL,
+	[MongoId] [varchar](100) NOT NULL,
+	[lookUpType] [varchar](200) NULL,
+	[Name] [varchar](300) NULL
+ CONSTRAINT [PK_RPT_FreqWhenLookUp] PRIMARY KEY CLUSTERED 
+(
+	[FreqWhenId] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+CREATE TABLE [dbo].[RPT_PatientAllergy](
+	[PatientAllergyId] [int] IDENTITY(1,1) NOT NULL,
+	[MongoId] [varchar](50) NOT NULL,
+	[AllergyId] [int] NOT NULL,
+	[MongoAllergyId] [varchar](50) NOT NULL,
+	[PatientId] [int] NULL,
+	[MongoPatientId] [varchar](50) NOT NULL,
+	[SeverityId] [int] NULL,
+	[MongoSeverityId] [varchar](50) NULL,
+	[StatusId] [varchar](100) NOT NULL,
+	[SourceId] [varchar](50) NOT NULL,
+	[StartDate] [datetime] NULL,
+	[EndDate] [datetime] NULL,
+	[Notes] [varchar](5000) NULL,
+	[SystemName] [varchar](50) NULL,
+	[MongoUpdatedBy] [varchar](50) NULL,
+	[UpdatedBy] [int] NULL,
+	[LastUpdatedOn] [datetime] NULL,
+	[MongoRecordCreatedBy] [varchar](50) NULL,
+	[RecordCreatedBy] [int] NULL,
+	[RecordCreatedOn] [datetime] NULL,
+	[Version] [float] NULL,
+	[TTLDate] [datetime] NULL,
+	[Delete] [varchar](50) NULL,
+	[ExtraElements] [varchar](5000) NULL,
+ CONSTRAINT [PK_PatientAllergy_1] PRIMARY KEY CLUSTERED 
+(
+	[PatientAllergyId] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON, FILLFACTOR = 90) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+CREATE TABLE [dbo].[RPT_PatientAllergyReaction](
+	[AllergyReactionId] [int] IDENTITY(1,1) NOT NULL,
+	[PatientAllergyId] [int] NOT NULL,
+	[MongoPatientAllergyId] [varchar](50) NULL,
+	[MongoReactionId] [varchar](50) NOT NULL,
+	[ReactionId] [int] NULL,
+	[MongoUpdatedBy] [varchar](50) NULL,
+	[UpdatedById] [int] NULL,
+	[LastUpdatedOn] [datetime] NULL,
+	[MongoRecordCreatedBy] [varchar](50) NULL,
+	[RecordCreatedById] [int] NULL,
+	[RecordCreatedOn] [datetime] NULL,
+	[Version] [float] NULL,
+ CONSTRAINT [PK_RPT_PatientAllergyReaction] PRIMARY KEY CLUSTERED 
+(
+	[AllergyReactionId] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+CREATE TABLE [dbo].[RPT_Allergy](
+	[AllergyId] [int] IDENTITY(1,1) NOT NULL,
+	[MongoId] [varchar](50) NOT NULL,
+	[Name] [varchar](100) NULL,
+	[CodingSystem] [varchar](50) NOT NULL,
+	[CodingSystemCode] [varchar](50) NOT NULL,
+	[Version] [float] NULL,
+	[MongoUpdatedBy] [varchar](50) NULL,
+	[UpdatedById] [int] NULL,
+	[LastUpdatedOn] [datetime] NULL,
+	[MongoRecordCreatedBy] [varchar](50) NULL,
+	[RecordCreatedById] [int] NULL,
+	[RecordCreatedOn] [datetime] NULL,
+	[TTLDate] [datetime] NULL,
+	[Delete] [varchar](50) NULL,
+	[ExtraElements] [varchar](5000) NULL,
+ CONSTRAINT [PK_RPT_Allergy] PRIMARY KEY CLUSTERED 
+(
+	[AllergyId] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO	
+CREATE TABLE [dbo].[RPT_AllergyType](
+	[AllergyTypeId] [int] IDENTITY(1,1) NOT NULL,
+	[AllergyId] [int] NULL,	
+	[MongoAllergyId] [varchar](50) NOT NULL,
+	[MongoTypeId] [varchar](50) NOT NULL,
+	[MongoUpdatedBy] [varchar](50) NULL,
+	[UpdatedById] [int] NULL,
+	[LastUpdatedOn] [datetime] NULL,
+	[MongoRecordCreatedBy] [varchar](50) NULL,
+	[RecordCreatedById] [int] NULL,
+	[RecordCreatedOn] [datetime] NULL,
+	[Version] [float] NULL,
+ CONSTRAINT [PK_RPT_AllergyType] PRIMARY KEY CLUSTERED 
+(
+	[AllergyTypeId] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+CREATE TABLE [dbo].[RPT_PatientMedSupp](
+	[PatientMedSuppId]		[int] IDENTITY(1,1) NOT NULL,
+	[MongoId]				[varchar](50) NOT NULL,
+	[PatientId]				[int] NOT NULL,
+	[MongoPatientId]		[varchar](50) NOT NULL,
+	[MongoFamilyId]			[varchar](50) NOT NULL,
+	[Name]					[varchar] (200) NOT NULL,
+	[Category]				[varchar] (200) NOT NULL,
+	[MongoTypeId]			[varchar] (50) NOT NULL,
+	[TypeId]				int NOT NULL,
+	[Status]				[varchar] (200) NOT NULL,
+	[Dosage]				[varchar] (500) NULL,
+	[Strength]				[varchar] (200) NULL,
+	[Route]					[varchar] (200) NULL,
+	[Form]					[varchar] (200) NULL,
+	[FreqQuantity]			[varchar] (200) NULL,
+	[MongoFreqHowOftenId]	[varchar] (50) NULL,
+	[FreqHowOftenId]		INT NULL,
+	[MongoFreqWhenId]		[varchar] (50) NULL,
+	[FreqWhenId]			INT NULL,
+	[MongoSourceId]			[varchar](50) NOT NULL,
+	[SourceId]				INT NULL,	
+	[StartDate]				[datetime] NULL,
+	[EndDate]				[datetime] NULL,
+	[Reason]				[varchar](5000) NULL,
+	[Notes]					[varchar](5000) NULL,	
+	[PrescribedBy]			[varchar](500) NULL,	
+	[SystemName]			[varchar](50) NULL,
+	[MongoUpdatedBy]		[varchar](50) NULL,
+	[UpdatedBy]				[int] NULL,
+	[LastUpdatedOn]			[datetime] NULL,
+	[MongoRecordCreatedBy]	[varchar](50) NULL,
+	[RecordCreatedBy]		[int] NULL,
+	[RecordCreatedOn]		[datetime] NULL,
+	[Version]				[float] NULL,
+	[TTLDate]				[DATETIME] NULL,
+	[Delete]				[VARCHAR](50) NULL
+ CONSTRAINT [PK_PatientMedSupp] PRIMARY KEY CLUSTERED 
+(
+	[PatientMedSuppId] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON, FILLFACTOR = 90) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+CREATE TABLE [dbo].[RPT_PatientMedSuppNDC](
+	[NDCId] [int] IDENTITY(1,1) NOT NULL,
+	[PatientMedSuppId] [int] NOT NULL,
+	[MongoPatientMedSuppId] [varchar](50) NOT NULL,
+	[NDC] [varchar] (200) NOT NULL
+ CONSTRAINT [PK_PatientMedSuppNDC] PRIMARY KEY CLUSTERED 
+(
+	[NDCId] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON, FILLFACTOR = 90) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+CREATE TABLE [dbo].[RPT_PatientMedSuppPhClass](
+	[PhCId] [int] IDENTITY(1,1) NOT NULL,
+	[PatientMedSuppId] [int] NOT NULL,
+	[MongoPatientMedSuppId] [varchar](50) NOT NULL,
+	[PharmClass] [varchar] (2000) NOT NULL
+ CONSTRAINT [PK_RPT_PatientMedSuppPhClass] PRIMARY KEY CLUSTERED 
+(
+	[PhCId] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON, FILLFACTOR = 90) ON [PRIMARY]
+) ON [PRIMARY]
+GO
---
+++ Reporting/Development/Current/Reporting/EngageReporting/ETL_Scripts/Program_Enrollment_Functions.sql
@@ -0,0 +1,96 @@
+﻿IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[fn_RPT_Did_Not_Enroll_Reason]') AND type in (N'FN', N'IF', N'TF', N'FS', N'FT'))
+DROP FUNCTION [dbo].[fn_RPT_Did_Not_Enroll_Reason]
+GO
+
+CREATE FUNCTION [dbo].[fn_RPT_Did_Not_Enroll_Reason] 
+(	
+	@patientid INT,
+	@patientprogramid INT
+)
+RETURNS VARCHAR(2000) 
+AS
+BEGIN
+	DECLARE @DidNotEnrollReason varchar(2000)
+
+	set @DidNotEnrollReason = 
+		( 		
+			COALESCE(
+				(SELECT 
+					( CASE WHEN pr12.[delete] = 'False' AND pa12.[State] = 'Completed' 
+							THEN pr12.[Text] ELSE NULL END) AS [didnotenrollreasonText]
+					FROM
+					RPT_Patient AS pt12 with (nolock) INNER JOIN
+					RPT_PatientProgram AS pp12 with (nolock) ON pt12.PatientId = pp12.PatientId INNER JOIN
+					RPT_PatientProgramModule AS pm12 with (nolock) ON pp12.PatientProgramId = pm12.PatientProgramId INNER JOIN
+					RPT_PatientProgramAction AS pa12 with (nolock) ON pm12.PatientProgramModuleId = pa12.PatientProgramModuleId INNER JOIN
+					RPT_PatientProgramStep AS ps12 with (nolock) ON pa12.ActionId = ps12.ActionId LEFT OUTER JOIN
+					RPT_PatientProgramResponse AS pr12 with (nolock) ON ps12.StepId = pr12.StepId
+					WHERE
+					pp12.sourceid = '541943a6bdd4dfa5d90002da'
+					AND ((ps12.SourceId = '53f4f885ac80d30e00000065') OR (ps12.SourceId = '532c3fc2f8efe368860003b5') )
+					AND ((pa12.SourceId = '53f4fd75ac80d30e00000083') OR (pa12.SourceId = '532c45bff8efe36886000446') )
+					AND (pp12.[Delete] = 'False')
+					AND (pa12.Archived = 'False')
+					AND (pr12.Selected = 'True')
+					AND pa12.ArchivedDate IS NULL
+					AND pt12.patientid = @patientid
+					AND pa12.[State] IN ('Completed')
+					AND pp12.patientprogramid = @patientprogramid
+					),
+					(SELECT TOP 1
+					(CASE WHEN pr13.[delete] = 'False' AND pa13.[State] = 'Completed'
+						THEN
+							pr13.[Text]
+						ELSE
+							NULL
+						END) AS [didnotenrollTextarch]
+					FROM
+					RPT_Patient AS pt13 with (nolock) INNER JOIN
+					RPT_PatientProgram AS pp13 with (nolock) ON pt13.PatientId = pp13.PatientId INNER JOIN
+					RPT_PatientProgramModule AS pm13 with (nolock) ON pp13.PatientProgramId = pm13.PatientProgramId INNER JOIN
+					RPT_PatientProgramAction AS pa13 with (nolock) ON pm13.PatientProgramModuleId = pa13.PatientProgramModuleId INNER JOIN
+					RPT_PatientProgramStep AS ps13 with (nolock) ON pa13.ActionId = ps13.ActionId LEFT OUTER JOIN
+					RPT_PatientProgramResponse AS pr13 with (nolock) ON ps13.StepId = pr13.StepId
+					WHERE
+					pp13.sourceid = '541943a6bdd4dfa5d90002da'
+					AND ((ps13.SourceId = '53f4f885ac80d30e00000065') OR (ps13.SourceId = '532c3fc2f8efe368860003b5') )
+					AND ((pa13.SourceId = '53f4fd75ac80d30e00000083') OR (pa13.SourceId = '532c45bff8efe36886000446') )
+					AND (pp13.[Delete] = 'False')
+					AND (pa13.Archived = 'True')
+					AND (pr13.Selected = 'True')
+					AND pa13.ArchivedDate IS NOT NULL
+					AND pa13.[State] IN ('Completed')
+					AND pt13.PatientId = @patientid
+					AND pp13.patientprogramid = @patientprogramid
+					AND pa13.ActionId IN (
+						SELECT DISTINCT 
+							pa14.ActionId
+						FROM
+							RPT_Patient AS pt14 with (nolock) INNER JOIN
+							RPT_PatientProgram AS pp14 with (nolock) ON pt14.PatientId = pp14.PatientId INNER JOIN
+							RPT_PatientProgramModule AS pm14 with (nolock) ON pp14.PatientProgramId = pm14.PatientProgramId INNER JOIN
+							RPT_PatientProgramAction AS pa14 with (nolock) ON pm14.PatientProgramModuleId = pa14.PatientProgramModuleId INNER JOIN
+							RPT_PatientProgramStep AS ps14 with (nolock) ON pa14.ActionId = ps14.ActionId LEFT OUTER JOIN
+							RPT_PatientProgramResponse AS pr14 with (nolock) ON ps14.StepId = pr14.StepId
+						WHERE
+							pp14.sourceid = '541943a6bdd4dfa5d90002da'
+							AND ((ps14.SourceId = '53f4f885ac80d30e00000065') OR (ps14.SourceId = '532c3fc2f8efe368860003b5') )
+							AND ((pa14.SourceId = '53f4fd75ac80d30e00000083') OR (pa14.SourceId = '532c45bff8efe36886000446') )
+							AND (pp14.[Delete] = 'False')
+							AND (pr14.Selected = 'True')
+							AND pt14.PatientId = pt13.patientId
+							AND pa14.[State] IN ('Completed')
+							AND pp14.patientprogramid = @patientprogramid
+							)
+					ORDER BY pa13.ArchivedDate DESC
+					) 	
+					)
+		)
+
+	RETURN @DidNotEnrollReason
+	
+END
+
+GO
+
+
