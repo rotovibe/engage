68e7e59ffe219095d964eca29ad1d8bb01508d39
Charles Zipp <charles.zipp@phytel.com>
2015-03-03 22:45:47 +0000

    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Shared;C11129

---
+++ Phytel.Services/Nuget/Phytel.Services.API.Cache/PreRelease/Container/MongoCacheClientContainer.cs
@@ -0,0 +1,27 @@
+﻿using Phytel.Services.API.Cache.Mongo;
+using Phytel.Services.AppSettings;
+using Phytel.Services.IOC;
+using Phytel.Services.Mongo.Repository;
+
+namespace Phytel.Services.API.Cache
+{
+    public class MongoCacheClientContainer : ContainerDecorator
+    {
+        public const string ScopeName = "Phytel.Services.API.Cache.Mongo";
+
+        public MongoCacheClientContainer(ContainerBuilder containerBuilder)
+            : base(containerBuilder)
+        {
+        }
+
+        protected override void OnBuild(Funq.Container container)
+        {
+            container.Register<IUnitOfWorkMongo<CacheContext>>(ScopeName, c =>
+                new UnitOfWorkMongo<CacheContext>(
+                    c.Resolve<IAppSettingsProvider>().Get(Constants.AppSettingsKeys.ConnectionName, Constants.AppSettingDefaultValues.ConnectionName),
+                    false
+                    )
+                ).ReusedWithin(Funq.ReuseScope.Default);
+        }
+    }
+}
