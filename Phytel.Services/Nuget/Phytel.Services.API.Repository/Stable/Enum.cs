f202e88d42ea0e7baeeccbaaa58353a2894b409e
Charles Zipp <charles.zipp@phytel.com>
2015-03-10 14:18:46 +0000

    Branched from $/PhytelCode/Phytel.Net/Shared/Phytel.Services/Nuget/Phytel.Services.API.Repository/PreRelease
    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Shared;C11253

---
+++ Phytel.Services/Nuget/Phytel.Services.API.Repository/Stable/Enum.cs
@@ -0,0 +1,19 @@
+﻿namespace Phytel.Services.API.Repository
+{
+    public enum SelectExpressionGroupType
+    {
+        AND,
+        OR,
+        EQ
+    }
+
+    public enum SelectExpressionType
+    {
+        EQ,
+        NOTEQ,
+        LIKE,
+        NOTLIKE,
+        STARTWITH,
+        IN
+    }
+}
