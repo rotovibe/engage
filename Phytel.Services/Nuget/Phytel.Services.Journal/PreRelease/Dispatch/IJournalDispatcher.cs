8b117dec9af7a8a708703cc6f3a12c29495937f9
Charles Zipp <charles.zipp@phytel.com>
2015-03-27 17:51:24 +0000

    fixed issue where url was not being recorded.
    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Shared;C11684

--- Phytel.Services/Nuget/Phytel.Services.Journal/PreRelease/Dispatch/IJournalDispatcher.cs
+++ Phytel.Services/Nuget/Phytel.Services.Journal/PreRelease/Dispatch/IJournalDispatcher.cs
@@ -5,7 +5,7 @@ namespace Phytel.Services.Journal.Dispatch
 {
     public interface IJournalDispatcher
     {
-        JournalEntry Dispatch(State state, object requestDto = null, string actionId = null, string name = null, string product = null, string ipAddress = null, string verb = null, DateTime? timeUtc = null, string parentActionId = null, Exception exception = null);
+        JournalEntry Dispatch(State state, object requestDto = null, string actionId = null, string name = null, string product = null, string ipAddress = null, string url = null, string verb = null, DateTime? timeUtc = null, string parentActionId = null, Exception exception = null);
 
         void DispatchEntries(params JournalEntry[] entries);
 
