8998d7e0836473ba90675fa45b667d4d799732b8
Tony DiGiorgio <tony.digiorgio@phytel.com>
2015-04-23 18:17:37 +0000

    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Shared;C12487

--- Phytel.Services/Nuget/Phytel.Services.Mongo/MongoTest/Properties/AssemblyInfo.cs
+++ Phytel.Services/Nuget/Phytel.Services.Mongo/MongoTest/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Phytel.Services.Mongo")]
+[assembly: AssemblyTitle("MongoTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Phytel Inc.")]
-[assembly: AssemblyProduct("Phytel.Services.Mongo")]
-[assembly: AssemblyCopyright("Copyright © Phytel Inc. 2014")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MongoTest")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("bc2689d2-bd01-4896-baa6-69c7d979b82f")]
+[assembly: Guid("592af25a-d458-4af4-ac5e-086a42511b7c")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -30,6 +30,7 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
-[assembly: AssemblyVersion("1.4.0.0")]
-[assembly: AssemblyFileVersion("1.4.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0.0")]
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Phytel.Services/Nuget/Phytel.Services.Mongo/MongoTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MongoDB.Driver;
+
+namespace MongoTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            MongoDatabase db = Phytel.Services.Mongo.MongoService.Instance.GetDatabase("TEST", false);
+            MongoUser user = db.FindUser("tony");
+            Assert.IsFalse(user.Username == "tony");
+        }
+    }
+}
