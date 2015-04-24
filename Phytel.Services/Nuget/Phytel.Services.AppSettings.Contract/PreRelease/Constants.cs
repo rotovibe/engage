16a8f476a1e836174c108ce04ec1cb3fee62df38
Tony DiGiorgio <tony.digiorgio@phytel.com>
2015-04-24 14:41:24 +0000

    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Shared;C12558

--- Phytel.Services/Nuget/Phytel.Services.AppSettings.Contract/PreRelease/Constants.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Phytel.Services.AppSettings.Contract
-{
-    public static class Constants
-    {
-        public const string ContractPropertyAppSettingsProviderScopeName = "ContractProperty";
-    }
-}
