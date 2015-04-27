b11405336f14fab6239fd91b1e7e345737ac510d
Tony DiGiorgio <tony.digiorgio@phytel.com>
2015-04-27 15:17:24 +0000

    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Shared;C12641

--- Phytel.Services/Nuget/Phytel.Services.Mongo/PreRelease/Linq/IMongoEntity.cs
+++ Phytel.Services/Nuget/Phytel.Services.Mongo/PreRelease/Linq/IMongoEntity.cs
@@ -18,32 +18,10 @@ namespace Phytel.Services.Mongo.Linq
         [BsonDefaultValue(1.0)]
         double Version { get; set; }
 
-        [BsonElement("uby")]
-        [BsonIgnoreIfNull(true)]
-        ObjectId? UpdatedBy { get; set; }
-
-        [BsonElement("del")]
-        [BsonDefaultValue(false)]
-        bool DeleteFlag { get; set; }
-
         [BsonElement("ttl")]
         [BsonDefaultValue(null)]
         [BsonIgnoreIfNull(true)]
         [BsonDateTimeOptions(Kind = System.DateTimeKind.Utc)]
         DateTime? TTLDate { get; set; }
-
-        [BsonIgnoreIfNull(true)]
-        [BsonElement("uon")]
-        [BsonDateTimeOptions(Kind = System.DateTimeKind.Utc)]
-        DateTime? LastUpdatedOn { get; set; }
-
-        [BsonIgnoreIfNull(true)]
-        [BsonElement("rcby")]
-        ObjectId RecordCreatedBy { get; }
-
-        [BsonIgnoreIfNull(true)]
-        [BsonElement("rcon")]
-        [BsonDateTimeOptions(Kind = System.DateTimeKind.Utc)]
-        DateTime RecordCreatedOn { get; }
     }
 }
