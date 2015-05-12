c3b88b3f090cdc891bdab18600189938c25d4a8d
Mel Bobadilla <mel.bobadilla@phytel.com>
2015-05-12 17:52:19 +0000

    ENG-1215 - Remove unused fields from flat tables.
    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/Nightingale;C12991

---
+++ Reporting/Development/Current/Reporting/Scripts/1.0.1/ENG_1215.sql
@@ -0,0 +1,33 @@
+/**** RPT_CareBridge_Enrollment_Info ****/
+IF EXISTS(SELECT * FROM SYS.COLUMNS WHERE NAME = N'Primary_Physician' AND OBJECT_ID = OBJECT_ID(N'RPT_CareBridge_Enrollment_Info'))
+BEGIN
+	ALTER TABLE 
+		RPT_CareBridge_Enrollment_Info
+	DROP COLUMN 
+		[Primary_Physician];
+END
+
+IF EXISTS(SELECT * FROM SYS.COLUMNS WHERE NAME = N'Primary_Physician_Practice' AND OBJECT_ID = OBJECT_ID(N'RPT_CareBridge_Enrollment_Info'))
+BEGIN
+	ALTER TABLE 
+		RPT_CareBridge_Enrollment_Info 
+	DROP COLUMN 
+		[Primary_Physician_Practice];	
+END	
+
+/**** RPT_Engage_Enrollment_Info ****/
+IF EXISTS(SELECT * FROM SYS.COLUMNS WHERE NAME = N'Primary_Physician' AND OBJECT_ID = OBJECT_ID(N'RPT_Engage_Enrollment_Info'))
+BEGIN
+	ALTER TABLE 
+		RPT_Engage_Enrollment_Info 
+	DROP COLUMN 
+		[Primary_Physician];
+END
+
+IF EXISTS(SELECT * FROM SYS.COLUMNS WHERE NAME = N'Primary_Physician_Practice' AND OBJECT_ID = OBJECT_ID(N'RPT_Engage_Enrollment_Info'))
+BEGIN
+	ALTER TABLE 
+		RPT_Engage_Enrollment_Info 
+	DROP COLUMN 
+		[Primary_Physician_Practice];	
+END	
---
+++ Reporting/Development/Current/Reporting/Scripts/1.0.1/ENG_1215_Rollback.sql
@@ -0,0 +1,33 @@
+/**** RPT_CareBridge_Enrollment_Info ****/
+IF NOT EXISTS(SELECT * FROM SYS.COLUMNS WHERE NAME = N'Primary_Physician' AND OBJECT_ID = OBJECT_ID(N'RPT_CareBridge_Enrollment_Info'))
+BEGIN
+	ALTER TABLE 
+		RPT_CareBridge_Enrollment_Info
+	ADD 
+		[Primary_Physician] VARCHAR(1000) NULL;
+END
+
+IF NOT EXISTS(SELECT * FROM SYS.COLUMNS WHERE NAME = N'Primary_Physician_Practice' AND OBJECT_ID = OBJECT_ID(N'RPT_CareBridge_Enrollment_Info'))
+BEGIN
+	ALTER TABLE 
+		RPT_CareBridge_Enrollment_Info 
+	ADD
+		[Primary_Physician_Practice] VARCHAR(2000) NULL;	
+END	
+
+/**** RPT_Engage_Enrollment_Info ****/
+IF NOT EXISTS(SELECT * FROM SYS.COLUMNS WHERE NAME = N'Primary_Physician' AND OBJECT_ID = OBJECT_ID(N'RPT_Engage_Enrollment_Info'))
+BEGIN
+	ALTER TABLE 
+		RPT_Engage_Enrollment_Info 
+	ADD 
+		[Primary_Physician] VARCHAR(1000) NULL;
+END
+
+IF NOT EXISTS(SELECT * FROM SYS.COLUMNS WHERE NAME = N'Primary_Physician_Practice' AND OBJECT_ID = OBJECT_ID(N'RPT_Engage_Enrollment_Info'))
+BEGIN
+	ALTER TABLE 
+		RPT_Engage_Enrollment_Info 
+	ADD 
+		[Primary_Physician_Practice] VARCHAR(2000) NULL;	
+END
