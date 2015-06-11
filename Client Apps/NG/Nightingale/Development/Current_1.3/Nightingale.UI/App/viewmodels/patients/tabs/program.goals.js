ad02eacd17acd4fa637d6da37444006c05fa2fa7
Tal Ben-Avraham <Tal.Ben-Avraham@phytel.com>
2015-06-11 17:13:23 +0000

    Branched from $/Nightingale/Client Apps/NG/Nightingale/Development/Current
    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/Nightingale;C13697

---
+++ Client Apps/NG/Nightingale/Development/Current_1.3/Nightingale.UI/App/viewmodels/patients/tabs/program.goals.js
@@ -0,0 +1,15 @@
+﻿define([], function () {
+
+    var ctor = function () {
+        var self = this;
+    };
+
+    ctor.prototype.activate = function (settings) {
+        var self = this;
+        self.settings = settings;
+        self.tab = self.settings.tab;
+        self.activeProgram = self.settings.activeProgram;
+    };
+
+    return ctor;
+});
