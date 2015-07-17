7b3de6888782c655f72be63abe1660df407fc5fc
Tal Ben-Avraham <Tal.Ben-Avraham@phytel.com>
2015-07-17 19:52:51 +0000

    Branched from $/Nightingale/Client Apps/NG/Nightingale/Development/Current
    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/Nightingale;C14517

---
+++ Client Apps/NG/Nightingale/Development/MultiId_Edit/Nightingale.UI/Sandbox/index.js
@@ -0,0 +1,11 @@
+﻿define([], function () {
+
+    var sandbox = {
+        activate: activate
+    }
+    return sandbox;
+
+    function activate() {
+        console.log('Sandbox activated');
+    }
+});
