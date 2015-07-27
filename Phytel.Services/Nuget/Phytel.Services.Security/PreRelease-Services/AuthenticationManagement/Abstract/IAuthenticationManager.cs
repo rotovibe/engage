76b3b92d898db5ecfcfba575e31b8cbf995cf261
Tony DiGiorgio <tony.digiorgio@phytel.com>
2015-07-27 14:12:58 +0000

    Branched from $/PhytelCode/Phytel.Net/Shared/Phytel.Services/Nuget/Phytel.Services.Security/PreRelease to support new Nuget Packages for Phytel Services
    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Shared;C14726

---
+++ Phytel.Services/Nuget/Phytel.Services.Security/PreRelease-Services/AuthenticationManagement/Abstract/IAuthenticationManager.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Phytel.Services.Security
+{
+    public interface IAuthenticationManager
+    {
+        bool PassphraseIsValid(string key, string salt, string plainTextPassphraseAttempt);
+        AuthenticationData GenerateAuthenticationDataForPassphrase(string plainTextPassphrase);
+    }
+}
