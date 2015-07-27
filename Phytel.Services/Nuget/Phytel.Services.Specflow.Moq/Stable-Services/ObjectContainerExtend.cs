0b26f22bb45da522fc643052a30310d5032146a8
Tony DiGiorgio <tony.digiorgio@phytel.com>
2015-07-27 19:02:28 +0000

    Branched from $/PhytelCode/Phytel.Net/Shared/Phytel.Services/Nuget/Phytel.Services.Specflow.Moq/PreRelease-Services
    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Shared;C14784

---
+++ Phytel.Services/Nuget/Phytel.Services.Specflow.Moq/Stable-Services/ObjectContainerExtend.cs
@@ -0,0 +1,15 @@
+﻿using BoDi;
+using Moq;
+
+namespace Phytel.Services.Specflow.Moq
+{
+    public static class ObjectContainerExtend
+    {
+        public static void RegisterMock<T>(this IObjectContainer objectContainer)
+            where T : class
+        {
+            objectContainer.RegisterInstanceAs<Mock<T>>(new Mock<T>());
+            objectContainer.RegisterInstanceAs<T>(objectContainer.Resolve<Mock<T>>().Object);
+        }
+    }
+}
