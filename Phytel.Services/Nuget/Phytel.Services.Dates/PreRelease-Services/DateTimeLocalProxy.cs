2fca42e03b8a2d1e03476af352589a3371c98854
Tony DiGiorgio <tony.digiorgio@phytel.com>
2015-07-27 19:24:20 +0000

    Branched from $/PhytelCode/Phytel.Net/Shared/Phytel.Services/Nuget/Phytel.Services.Dates/PreRelease
    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Shared;C14793

---
+++ Phytel.Services/Nuget/Phytel.Services.Dates/PreRelease-Services/DateTimeLocalProxy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Phytel.Services.Dates
+{
+    public class DateTimeLocalProxy : DateTimeProxy, IDateTimeProxy
+    {
+        public DateTimeLocalProxy(string dateFormat = DateFormat, string dateTimeFormatString = DateTimeFormatString, string timeFormat = TimeFormat)
+            : base(dateFormat, dateTimeFormatString, timeFormat)
+        {
+        }
+
+        public override DateTime GetCurrentDate()
+        {
+            return DateTime.Today;
+        }
+
+        public override DateTime GetCurrentDateTime()
+        {
+            return DateTime.Now;
+        }
+    }
+}
