49f906c1b6dde671ee2705ac50394ff35e417ea5
Tony DiGiorgio <tony.digiorgio@phytel.com>
2015-07-27 19:25:52 +0000

    Branched from $/PhytelCode/Phytel.Net/Shared/Phytel.Services/Nuget/Phytel.Services.Dates/PreRelease-Services
    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Shared;C14795

---
+++ Phytel.Services/Nuget/Phytel.Services.Dates/Stable-Services/DateTimeUtcProxy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Phytel.Services.Dates
+{
+    public class DateTimeUtcProxy : DateTimeProxy, IDateTimeProxy
+    {
+        public DateTimeUtcProxy(string dateFormat = DateFormat, string dateTimeFormatString = DateTimeFormatString, string timeFormat = TimeFormat)
+            : base(dateFormat, dateTimeFormatString, timeFormat)
+        {
+        }
+
+        public override DateTime GetCurrentDate()
+        {
+            return DateTime.UtcNow.Date;
+        }
+
+        public override DateTime GetCurrentDateTime()
+        {
+            return DateTime.UtcNow;
+        }
+    }
+}
