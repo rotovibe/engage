25fdf537d599a5bfefaae1c14c14b2b340d11f22
Tony DiGiorgio <tony.digiorgio@phytel.com>
2015-07-27 18:29:48 +0000

    Branched from $/PhytelCode/Phytel.Net/Shared/Phytel.Services/Nuget/Phytel.Services.Serializer/PreRelease
    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Shared;C14764

---
+++ Phytel.Services/Nuget/Phytel.Services.Serializer/PreRelease-Services/ISerializer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Phytel.Services.Serializer
+{
+    public interface ISerializer
+    {
+        object Deserialize(string value, Type targetType);
+
+        T Deserialize<T>(string value);
+
+        string Serialize(object value, Type type);
+
+        string Serialize<T>(T value);
+    }
+}
