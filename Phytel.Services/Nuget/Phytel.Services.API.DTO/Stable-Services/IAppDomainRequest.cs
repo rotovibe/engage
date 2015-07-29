f439ffa0ec5bfdd9a1438f7d9eb32597b9532dce
Tony DiGiorgio <tony.digiorgio@phytel.com>
2015-07-29 12:58:01 +0000

    Branched from $/PhytelCode/Phytel.Net/Shared/Phytel.Services/Nuget/Phytel.Services.API.DTO/PreRelease-Services
    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Shared;C14893

---
+++ Phytel.Services/Nuget/Phytel.Services.API.DTO/Stable-Services/IAppDomainRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace Phytel.Services.API.DTO
+{
+    public interface IAppDomainRequest : IContractRequest, IVersionRequest
+    {
+        string ContractNumber { get; set; }
+
+        string Token { get; set; }
+
+        string UserId { get; set; }
+
+        double Version { get; set; }
+    }
+}
