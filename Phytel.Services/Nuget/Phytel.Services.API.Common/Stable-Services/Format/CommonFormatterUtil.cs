a9ba0595fde2d9f66959dbb4392f55019dcbbcfc
Tony DiGiorgio <tony.digiorgio@phytel.com>
2015-07-29 12:48:16 +0000

    Branched from $/PhytelCode/Phytel.Net/Shared/Phytel.Services/Nuget/Phytel.Services.API.Common/PreRelease-Services
    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Shared;C14890

---
+++ Phytel.Services/Nuget/Phytel.Services.API.Common/Stable-Services/Format/CommonFormatterUtil.cs
@@ -0,0 +1,36 @@
+﻿using Phytel.API.Interface;
+using ServiceStack.ServiceInterface.ServiceModel;
+using ServiceStack.ServiceHost;
+using System;
+using System.Net;
+
+namespace Phytel.API.Common.Format
+{
+    public class CommonFormatterUtil : ICommonFormatterUtil
+    {
+        public void FormatExceptionResponse<T>(T response, IHttpResponse httpResponse, Exception ex) where T : IDomainResponse
+        {
+            if (ex != null && (ex is UnauthorizedAccessException || ex.Message == "UnauthorizedAccessException" || ex.Message == "Unauthorized"))
+                httpResponse.StatusCode = (int) HttpStatusCode.Unauthorized;
+            else
+                httpResponse.StatusCode = (int) HttpStatusCode.InternalServerError;
+
+            response.Status = new ResponseStatus(ex.ToErrorCode(), ex.Message)
+            {
+                ErrorCode = ex.ToErrorCode(),
+                Message = ex.Message,
+                StackTrace = ex.StackTrace
+            };
+        }
+
+        public string FormatDateOfBirth(string val)
+        {
+            var result = string.Empty;
+            DateTime parsed;
+            if (DateTime.TryParse(val, out parsed))
+                result = val;
+
+            return result;
+        }
+    }
+}
