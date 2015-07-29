4f140e40d7a0fb47d219e3d7e26e0eefb483de63
Tony DiGiorgio <tony.digiorgio@phytel.com>
2015-07-29 13:04:38 +0000

    Branched from $/PhytelCode/Phytel.Net/Shared/Phytel.Services/Nuget/Phytel.Services.API.Filter/PreRelease
    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Shared;C14894

---
+++ Phytel.Services/Nuget/Phytel.Services.API.Filter/PreRelease-Services/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace Phytel.Services.API.Filter
+{
+    public static class Constants
+    {
+        public const string NamedStringContext = Phytel.Services.API.Provider.Constants.HostContextKeyContext;
+        public const string NamedStringContractNumber = Phytel.Services.API.Provider.Constants.HostContextKeyContractNumber;
+        public const string NamedStringVersion = Phytel.Services.API.Provider.Constants.HostContextKeyVersion;
+    }
+}
