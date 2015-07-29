85c7d4c060ac4e607ca8cdd8e6889e770957ce3e
Tony DiGiorgio <tony.digiorgio@phytel.com>
2015-07-29 02:06:45 +0000

    Branched from $/PhytelCode/Phytel.Net/Shared/Phytel.Services/Nuget/Phytel.Services.API.Common/PreRelease
    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Shared;C14888

---
+++ Phytel.Services/Nuget/Phytel.Services.API.Common/PreRelease-Services/Format/ICommonFormatterUtil.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Phytel.API.Common.Format
+{
+    public interface ICommonFormatterUtil
+    {
+        string FormatDateOfBirth(string val);
+        void FormatExceptionResponse<T>(T response, ServiceStack.ServiceHost.IHttpResponse httpResponse, Exception ex) where T : Phytel.API.Interface.IDomainResponse;
+    }
+}
