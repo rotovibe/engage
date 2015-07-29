9ba2697919ae9842fe994665e4c486efa868f18e
Tony DiGiorgio <tony.digiorgio@phytel.com>
2015-07-29 01:19:10 +0000

    Branched from $/PhytelCode/Phytel.Net/Shared/Phytel.Services/Nuget/Phytel.Services.AppSettings/PreRelease-Services
    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Shared;C14873

---
+++ Phytel.Services/Nuget/Phytel.Services.AppSettings/Stable-Services/ConfigAppSettingsProvider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Specialized;
+using System.Configuration;
+
+namespace Phytel.Services.AppSettings
+{
+    public class ConfigAppSettingsProvider : EagerLoadedAppSettingsProvider
+    {
+        public ConfigAppSettingsProvider()
+            : this(ConfigurationManager.AppSettings)
+        {
+        }
+
+        public ConfigAppSettingsProvider(NameValueCollection appSettings)
+            : base(appSettings.ToDictionary())
+        {
+        }
+    }
+}
