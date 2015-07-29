1bc3c9be2dba23d247e7c6dfdf427020087781be
Tony DiGiorgio <tony.digiorgio@phytel.com>
2015-07-29 01:00:44 +0000

    Branched from $/PhytelCode/Phytel.Net/Shared/Phytel.Services/Nuget/Phytel.Services.Web/PreRelease-Services
    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Shared;C14870

---
+++ Phytel.Services/Nuget/Phytel.Services.Web/Stable-Services/Security/SqlRoleProvider.cs
@@ -0,0 +1,16 @@
+﻿using Phytel.Services.SQLServer;
+using System.Collections.Specialized;
+
+namespace Phytel.Services.Web.Security
+{
+    public class SqlRoleProvider : System.Web.Security.SqlRoleProvider
+    {
+        public override void Initialize(string name, NameValueCollection config)
+        {
+            string dbConnName = config["connectionStringName"];
+            config.Add("connectionString", SQLDataService.Instance.GetConnectionString(dbConnName, false));
+
+            base.Initialize(name, config);
+        }
+    }
+}
