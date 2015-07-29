960dcb82236f2a4432273ca81fc05643f8d775f5
Tony DiGiorgio <tony.digiorgio@phytel.com>
2015-07-29 00:39:41 +0000

    Branched from $/PhytelCode/Phytel.Net/Shared/Phytel.Services/Nuget/Phytel.Services.Mongo.Repository/PreRelease
    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Shared;C14866

---
+++ Phytel.Services/Nuget/Phytel.Services.Mongo.Repository/PreRelease-Services/Abstract/Interfaces/IUnitOfWorkMongo.cs
@@ -0,0 +1,11 @@
+﻿using Phytel.Services.Mongo.Linq;
+using System;
+
+namespace Phytel.Services.Mongo.Repository
+{
+    public interface IUnitOfWorkMongo<TContext> : IDisposable
+        where TContext : MongoContext
+    {
+        TContext MongoContext { get; }
+    }
+}
