9288baf6197b406298131ada805d7bf975dd03f4
Tony DiGiorgio <tony.digiorgio@phytel.com>
2015-07-29 01:50:47 +0000

    Branched from $/PhytelCode/Phytel.Net/Shared/Phytel.Services/Nuget/Phytel.Services.API.Cache/PreRelease
    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Shared;C14885

---
+++ Phytel.Services/Nuget/Phytel.Services.API.Cache/PreRelease-Services/Mongo/CacheContext.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using Phytel.Services.Mongo.Linq;
+
+namespace Phytel.Services.API.Cache.Mongo
+{
+    public class CacheContext : MongoContext
+    {
+        public CacheContext(string connectionName, bool isContract)
+            : base(connectionName, isContract)
+        {
+            Cache = new MongoSet<CacheMongoEntity, ObjectId>(this);
+        }
+
+        public MongoSet<CacheMongoEntity, ObjectId> Cache { get; private set; }
+    }
+}
