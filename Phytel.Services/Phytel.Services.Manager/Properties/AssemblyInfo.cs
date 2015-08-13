8cf0959eca83c279bc8492b20ce2a19e36fdaf7d
Tony DiGiorgio <tony.digiorgio@phytel.com>
2015-08-13 14:52:52 +0000

    Updated to 2.0 versions of referenced assemblies to support Mongo 1.10 driver
    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Shared;C15283

--- Phytel.Services/Phytel.Services.Manager/Properties/AssemblyInfo.cs
+++ Phytel.Services/Phytel.Services.Manager/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
-[assembly: AssemblyVersion("1.3.0.1")]
-[assembly: AssemblyFileVersion("1.3.0.1")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
