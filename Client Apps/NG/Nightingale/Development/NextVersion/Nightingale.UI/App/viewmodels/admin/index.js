bdd7095a2f994b658d6670fca8819871823f148b
Tal Ben-Avraham <Tal.Ben-Avraham@phytel.com>
2015-08-26 20:47:16 +0000

    Branched from $/Nightingale/Client Apps/NG/Nightingale/Development/Current
    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/Nightingale;C15516

---
+++ Client Apps/NG/Nightingale/Development/NextVersion/Nightingale.UI/App/viewmodels/admin/index.js
@@ -0,0 +1,17 @@
+﻿//TODO: Inject dependencies
+define(['services/navigation', 'services/session', 'config.services', 'services/datacontext'],
+    function (navigation, session, servicesConfig, datacontext) {
+        
+        var vm = {
+            activate: activate,
+            navigation: navigation,
+            title: 'Admin'
+        };
+
+        return vm;
+        
+        function activate() {
+            console.log('Loaded admin index')
+        }
+
+    });
