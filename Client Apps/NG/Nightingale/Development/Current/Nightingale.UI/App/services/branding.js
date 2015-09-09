4047e7155e95635e1355fff605e986f267b8d564
Patrick Walters <patrick.walters@phytel.com>
2015-09-09 18:00:59 +0000

    Add the branding changes for Watson Health CM back in
    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/Nightingale;C15795

--- Client Apps/NG/Nightingale/Development/Current/Nightingale.UI/App/services/branding.js
+++ Client Apps/NG/Nightingale/Development/Current/Nightingale.UI/App/services/branding.js
@@ -1,16 +1,30 @@
-﻿define([],
-    function () {
+﻿define(['services/session'],
+  function (session) {
 
-        var currentBrand = ko.observable(new brand('Phytel', '/NightingaleUI/Content/images/phytel_logo.png'));
-
-        var branding = {
-            currentBrand: currentBrand
-        };
-        return branding;
-
-        function brand(name, logoPath) {
-            var self = this;
-            self.brandName = ko.observable(name);
-            self.imageSource = ko.observable(logoPath);
+    var currentBrand = ko.computed(function() {
+      var returnValue;
+      var brandName = 'Phytel';
+      var logoLink = '/NightingaleUI/Content/images/phytel_logo.png';
+      var classOverride = 'phytel-logo';
+      if (session.currentUser()) {
+        if (session.currentUser().contracts()[0].number() === 'Demo001') {
+          brandName = 'Watson Health';
+          logoLink = '/NightingaleUI/Content/images/watson_cm_logo.png';
+          classOverride = 'watson-logo';
         }
+      }
+      return new brand(brandName, logoLink, classOverride);
     });
+
+    var branding = {
+      currentBrand: currentBrand
+    };
+    return branding;
+
+    function brand(name, logoPath, override) {
+      var self = this;
+      self.brandName = ko.observable(name);
+      self.imageSource = ko.observable(logoPath);
+      self.classOverride = ko.observable(override)
+    }
+});
