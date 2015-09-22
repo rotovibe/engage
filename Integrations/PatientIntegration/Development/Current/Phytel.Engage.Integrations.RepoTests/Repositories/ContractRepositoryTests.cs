5090dd8a54e66285f48faabd72c1b479e2541c37
Mel Bobadilla <mel.bobadilla@phytel.com>
2015-09-22 04:51:19 +0000

    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/Nightingale;C16087

--- Integrations/PatientIntegration/Development/Current/Phytel.Engage.Integrations.RepoTests/Repositories/ContractRepositoryTests.cs
+++ Integrations/PatientIntegration/Development/Current/Phytel.Engage.Integrations.RepoTests/Repositories/ContractRepositoryTests.cs
@@ -20,4 +20,5 @@ namespace Phytel.Engage.Integrations.Repo.RepositoriesTests
             cr.SelectAll(); 
         }
     }
+
 }
---
+++ Integrations/PatientIntegration/Development/Current/Phytel.Engage.Integrations.RepoTests/Repositories/XrefContractRepositoryTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Phytel.Engage.Integrations.Repo.Repositories;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Phytel.Engage.Integrations.Repo.Connections;
+using Phytel.Engage.Integrations.Repo.DTO;
+
+namespace Phytel.Engage.Integrations.Repo.Repositories.Tests
+{
+    [TestClass()]
+    public class XrefContractRepositoryTests
+    {
+        [TestMethod()]
+        public void SelectAllTest()
+        {
+            var cr = new XrefContractRepository("ORLANDOHEALTH001") { ConnStr = new SQLConnectionProvider() };
+            var result = cr.SelectAll();
+            Assert.AreEqual(typeof(List<PatientXref>), result.GetType());
+        }
+    }
+}
