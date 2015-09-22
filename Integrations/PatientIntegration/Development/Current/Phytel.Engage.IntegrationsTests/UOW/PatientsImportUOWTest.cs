f7e00f5870ded60425f755f1bb2cacee0f158cf5
Mel Bobadilla <mel.bobadilla@phytel.com>
2015-09-22 04:51:04 +0000

    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/Nightingale;C16086

---
+++ Integrations/PatientIntegration/Development/Current/Phytel.Engage.IntegrationsTests/UOW/PatientsImportUOWTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Phytel.Engage.Integrations.UOW;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Phytel.API.DataDomain.Patient.DTO;
+
+namespace Phytel.Engage.Integrations.UOW.Tests
+{
+    [TestClass()]
+    public class PatientsImportUOWTest
+    {
+        [TestMethod()]
+        public void Commit()
+        {
+            var uow = new PatientsImportUow<PatientData>
+            {
+                Pocos =
+                    new List<PatientData>
+                    {
+                        new PatientData {FirstName = "Ted"},
+                        new PatientData {FirstName = "Mammy"},
+                        new PatientData {FirstName = "Lisa"},
+                        new PatientData {FirstName = "George"},
+                        new PatientData {FirstName = "xavier"},
+                        new PatientData {FirstName = "pablo"},
+                        new PatientData {FirstName = "jimmy"},
+                        new PatientData {FirstName = "eric"},
+                        new PatientData {FirstName = "jane"},
+                        new PatientData {FirstName = "bobby"}
+                    }
+            };
+
+            uow.Commit();
+        }
+    }
+}
