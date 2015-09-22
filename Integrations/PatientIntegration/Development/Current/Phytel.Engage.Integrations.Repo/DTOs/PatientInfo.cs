c1107a731fdba50a2918c7138afc20958ef463c6
Mel Bobadilla <mel.bobadilla@phytel.com>
2015-09-22 04:52:20 +0000

    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/Nightingale;C16092

--- Integrations/PatientIntegration/Development/Current/Phytel.Engage.Integrations.Repo/DTOs/PatientInfo.cs
+++ Integrations/PatientIntegration/Development/Current/Phytel.Engage.Integrations.Repo/DTOs/PatientInfo.cs
@@ -1,15 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Phytel.Engage.Integrations.Repo.DTOs
 {
     public class PatientInfo
     {
-        public string GroupName { get; set; }
-        public int? GroupId { get; set; }
         public int? SubscriberId { get; set; }
         public int? PatientId { get; set; }
         public string FirstName { get; set; }
@@ -18,7 +12,12 @@ namespace Phytel.Engage.Integrations.Repo.DTOs
         public string Suffix { get; set; }
         public string Gender { get; set; }
         public DateTime? BirthDate { get; set; }
-        public string SSN { get; set; }
+        public string Ssn { get; set; }
+        public string Phone { get; set; }
+        public int? Priority { get; set; }
+        public string PCP { get; set; }
+        public Guid? CareManagerId { get; set; }
+        public string CareManagerName { get; set; }
         public DateTime? CreateDate { get; set; }
         public DateTime? UpdateDate { get; set; }
         public string Status { get; set; }
---
+++ Integrations/PatientIntegration/Development/Current/Phytel.Engage.Integrations.Repo/DTOs/PatientXref.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Phytel.Engage.Integrations.Repo.DTO
+{
+    public class PatientXref
+    {
+        public int ID { get; set; }
+        public int? PhytelPatientID { get; set; }
+        public string ExternalDisplayPatientId { get; set; }
+        public string ExternalPatientID { get; set; }
+        public string SendingApplication { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime? UpdateDate { get; set; }
+        public int? UpdatedBy { get; set; }
+    }
+}
