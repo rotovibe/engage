d24390b2a0bf28e29d013e95009f3a813bddd0c0
Snehal Ingavale <Snehal.Ingavale@phytel.com>
2015-11-23 21:48:25 +0000

    Updating startdate, enddate in RPT_Medication table
    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/Nightingale;C17627

--- Reporting/Development/Current/Reporting/Scripts/Releases/1.0.4/ENG-1500.sql
+++ Reporting/Development/Current/Reporting/Scripts/Releases/1.0.4/ENG-1500.sql
@@ -22,10 +22,36 @@ ALTER TABLE [dbo].[RPT_Flat_MedicationMap_Dim]
 ALTER COLUMN EndDate datetime NULL
 GO
 
+DROP INDEX [IX_RPT_Medication_FamilyId] ON [dbo].[RPT_Medication]
+GO
+
+UPDATE [dbo].[RPT_Medication]
+SET StartDate = null
+WHERE isdate(StartDate) = 0
+GO
+
 ALTER TABLE [dbo].[RPT_Medication]
 ALTER COLUMN StartDate datetime NULL
 GO
 
 ALTER TABLE [dbo].[RPT_Medication]
 ALTER COLUMN EndDate datetime NULL
+GO
+
+CREATE NONCLUSTERED INDEX [IX_RPT_Medication_FamilyId] ON [dbo].[RPT_Medication]
+(
+	[FamilyId] ASC,
+	[ProductId] ASC,
+	[NDC] ASC,
+	[ProprietaryName] ASC,
+	[StartDate] ASC,
+	[EndDate] ASC,
+	[Version] ASC,
+	[DeleteFlag] ASC,
+	[TTLDate] ASC,
+	[LastUpdatedOn] ASC,
+	[MongoRecordCreatedBy] ASC,
+	[RecordCreatedOn] ASC,
+	[MongoUpdatedBy] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 GO
--- Reporting/Development/Current/Reporting/Scripts/Releases/1.0.4/ENG-1500_Rollback.sql
+++ Reporting/Development/Current/Reporting/Scripts/Releases/1.0.4/ENG-1500_Rollback.sql
@@ -22,10 +22,31 @@ ALTER TABLE [dbo].[RPT_Flat_MedicationMap_Dim]
 ALTER COLUMN EndDate [varchar](25) NULL
 GO
 
+DROP INDEX [IX_RPT_Medication_FamilyId] ON [dbo].[RPT_Medication]
+GO
+
 ALTER TABLE [dbo].[RPT_Medication]
 ALTER COLUMN StartDate [varchar](100) NULL
 GO
 
 ALTER TABLE [dbo].[RPT_Medication]
 ALTER COLUMN EndDate [varchar](100) NULL
+GO
+
+CREATE NONCLUSTERED INDEX [IX_RPT_Medication_FamilyId] ON [dbo].[RPT_Medication]
+(
+	[FamilyId] ASC,
+	[ProductId] ASC,
+	[NDC] ASC,
+	[ProprietaryName] ASC,
+	[StartDate] ASC,
+	[EndDate] ASC,
+	[Version] ASC,
+	[DeleteFlag] ASC,
+	[TTLDate] ASC,
+	[LastUpdatedOn] ASC,
+	[MongoRecordCreatedBy] ASC,
+	[RecordCreatedOn] ASC,
+	[MongoUpdatedBy] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 GO
