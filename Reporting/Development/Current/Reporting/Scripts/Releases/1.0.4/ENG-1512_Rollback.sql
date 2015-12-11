70e06b90caf7adf37548e7760443921a2d5c946a
Snehal Ingavale <Snehal.Ingavale@phytel.com>
2015-12-11 21:10:28 +0000

    fxing rollback
    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/Nightingale;C17923

--- Reporting/Development/Current/Reporting/Scripts/Releases/1.0.4/ENG-1512_Rollback.sql
+++ Reporting/Development/Current/Reporting/Scripts/Releases/1.0.4/ENG-1512_Rollback.sql
@@ -1,4 +1,4 @@
-IF NOT EXISTS (SELECT * FROM sys.columns WHERE Name IN (N'StartTime', N'Duration') AND object_id = Object_ID(N'[dbo].RPT_ToDo'))
+IF EXISTS (SELECT * FROM sys.columns WHERE Name IN (N'StartTime', N'Duration') AND object_id = Object_ID(N'[dbo].RPT_ToDo'))
 	BEGIN
 		ALTER TABLE RPT_ToDo
 		DROP COLUMN [StartTime], COLUMN [Duration]
@@ -6,7 +6,7 @@ IF NOT EXISTS (SELECT * FROM sys.columns WHERE Name IN (N'StartTime', N'Duration
 GO
 
 
-IF NOT EXISTS (SELECT * FROM sys.columns WHERE Name IN (N'StartTime', N'Duration') AND object_id = Object_ID(N'[dbo].RPT_Dim_ToDo'))
+IF EXISTS (SELECT * FROM sys.columns WHERE Name IN (N'StartTime', N'Duration') AND object_id = Object_ID(N'[dbo].RPT_Dim_ToDo'))
 	BEGIN
 		ALTER TABLE RPT_Dim_ToDo
 		DROP COLUMN [StartTime], COLUMN [Duration]
