c0af5d39f67753b6d1090f6d5c66674d0da590ea
Dondre Lockett <Dondre.Lockett2@phytel.com>
2016-01-21 18:08:15 +0000

    CORD-2102 - Removed TotalPage from PaginationResponse
    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Shared;C18813

--- Phytel.Services/Nuget/Phytel.Services.API.Pagination/PreRelease/IPaginationResponse.cs
+++ Phytel.Services/Nuget/Phytel.Services.API.Pagination/PreRelease/IPaginationResponse.cs
@@ -3,7 +3,6 @@
     public interface IPaginationResponse
     {
         long? TotalCount { get; set; }
-        long? TotalPages { get; set; }
         int? Take { get; set; }
         int Skip { get; set; }
     }
