1e9666aa17e854a540cac438c096ca6396d26974
Josh Gattis <Josh.Gattis@phytel.com>
2016-01-22 19:10:24 +0000

    Adding TextResult to the component
    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Shared;C18867

---
+++ Phytel.Services/Nuget/Phytel.Services.Communication/PreRelease/Phytel.Services.Communication/Model/TextResult.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Phytel.Services.Communication.Model
+{
+    public class TextResult
+    {
+        public string ReferenceID { get; set; }
+        public string ResultCode { get; set; }
+        public string ResultStatus { get; set; }
+        public string ResultDescription { get; set; }
+        public string ErrorCode { get; set; }
+        public string ErrorDescription { get; set; }
+        public string FromNumber { get; set; }
+        public string ToPhoneNumber { get; set; }
+        public DateTime? ProcessedDate { get; set; }
+        public string ResultType { get; set; }
+
+        /* Vendor Fields */
+        public string VendorTransactionID { get; set; }
+        public string VendorStatus { get; set; }
+        public string VendorErrorCode { get; set; }
+        public string VendorErrorDescription { get; set; }
+
+        /* These should be depreciated once we're using the Bus */
+        public string ApplicationURL { get; set; }
+    }
+}
