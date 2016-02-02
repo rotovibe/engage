828819d836468d41ee82fe70e9328d87f39d3a69
Biju Joseph <biju.joseph@phytel.com>
2016-02-02 20:18:57 +0000

    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/Nightingale;C19150

--- Reporting/Development/Current/Reporting/Scripts/Releases/1.0.5/ENG-1590_sprocs_Rollback.sql
+++ Reporting/Development/Current/Reporting/Scripts/Releases/1.0.5/ENG-1590_sprocs_Rollback.sql
@@ -1,3 +1,3 @@
-IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[spPhy_RPT_Flat_GoalStatistics]') AND type in (N'P', N'PC'))
-DROP PROCEDURE [dbo].[spPhy_RPT_Flat_GoalStatistics]
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[spPhy_RPT_Flat_InterventionStatistics]') AND type in (N'P', N'PC'))
+DROP PROCEDURE [dbo].[spPhy_RPT_Flat_InterventionStatistics]
 GO
--- Reporting/Development/Current/Reporting/Scripts/Releases/1.0.5/ENG-1590_structs_rollback.sql
+++ Reporting/Development/Current/Reporting/Scripts/Releases/1.0.5/ENG-1590_structs_rollback.sql
@@ -1,4 +1,3 @@
-
-IF EXISTS (SELECT * FROM sys.objects WHERE type = 'U' AND name = 'RPT_Flat_GoalStatistics')
-	DROP TABLE [dbo].[RPT_Flat_GoalStatistics]
+IF EXISTS (SELECT * FROM sys.objects WHERE type = 'U' AND name = 'RPT_Flat_InterventionStatistics')
+	DROP TABLE [dbo].[RPT_Flat_InterventionStatistics]
 GO
