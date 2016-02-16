126560c2f20a8d6bb553021ff6634a741b9dd6c5
Mel Bobadilla <mel.bobadilla@phytel.com>
2016-02-16 22:28:57 +0000

    Branched from $/PhytelCode/Phytel.Net/Services/API/DataDomain/Phytel.API.DataDomain.Contact/Development/Current
    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Services;C19591

---
+++ API/DataDomain/Phytel.API.DataDomain.Contact/Development/Bristol-branch/Contact.Test/Stubs/StubHelpers.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using MongoDB.Bson;
+using Phytel.API.Common;
+
+namespace Phytel.API.DataDomain.Contact.Test.Stubs
+{
+    public class StubHelpers : IHelpers
+    {
+
+        public string BuildURL(string baseURL, string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object ConvertToAppropriateType(object p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<BsonValue> ConvertToBsonValueList(object p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ObjectId> ConvertToObjectIdList(List<string> stringList)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> ConvertToStringList(List<ObjectId> objectIds)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogException(int processId, Exception ex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string TrimAndLimit(string value, int limit)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SerializeObject<T>(T obj, string filePath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object DeserializeObject<T>(string filePath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ API/DataDomain/Phytel.API.DataDomain.Contact/Development/Bristol-branch/Contact/IContactRepositoryFactory.cs
@@ -0,0 +1,9 @@
+﻿using Phytel.API.Interface;
+using System;
+namespace Phytel.API.DataDomain.Contact
+{
+    public interface IContactRepositoryFactory
+    {
+        IContactRepository GetRepository(IDataDomainRequest request, RepositoryType type);
+    }
+}
