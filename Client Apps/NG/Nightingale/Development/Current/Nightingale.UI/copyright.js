3156588e9e5548582352eaaaa9a6048ed503b994
Tal Ben-Avraham <Tal.Ben-Avraham@phytel.com>
2016-02-19 22:54:41 +0000

    ENG-1681 adding copyrights to js sources using new gulp build
    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/Nightingale;C19665

---
+++ Client Apps/NG/Nightingale/Development/Current/Nightingale.UI/copyright.js
@@ -0,0 +1,6 @@
+/*!	@license 
+	Licensed Materials - Property of IBM
+	PID : 5725Z49
+	Copyright IBM Corporation 2013, 2016
+	US Government Users Restricted Rights- Use, duplication or disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
+*/
