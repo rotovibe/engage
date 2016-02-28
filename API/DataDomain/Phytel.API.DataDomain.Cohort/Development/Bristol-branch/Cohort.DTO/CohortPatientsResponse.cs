4877e3cc58632a6cb4da29d1649435b77e2b6c77
Mel Bobadilla <mel.bobadilla@phytel.com>
2016-02-28 22:45:35 +0000

    Branched from $/PhytelCode/Phytel.Net/Services/API/DataDomain/Phytel.API.DataDomain.Cohort/Development/Current
    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Services;C19917

---
+++ API/DataDomain/Phytel.API.DataDomain.Cohort/Development/Bristol-branch/Cohort.DTO/CohortPatientsResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Phytel.API.DataDomain.Cohort.DTO
+{
+    public class CohortPatientsResponse
+    {
+    }
+}
