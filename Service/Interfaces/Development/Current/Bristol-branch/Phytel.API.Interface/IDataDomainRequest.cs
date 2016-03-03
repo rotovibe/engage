cf3ec1fec6c1c58502981e5b783024cb34c9d447
Mel Bobadilla <mel.bobadilla@phytel.com>
2016-03-03 15:49:12 +0000

    Branched from $/PhytelCode/Phytel.Net/Services/API/Interfaces/Development/Current
    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/Nightingale;C20065

---
+++ Service/Interfaces/Development/Current/Bristol-branch/Phytel.API.Interface/IDataDomainRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phytel.API.Interface
+{
+    public interface IDataDomainRequest
+    {
+        double Version { get; set; }
+        string ContractNumber { get; set; }
+        string Context { get; set; }
+        string UserId { get; set; }
+    }
+}
