2c9d6d397f7388b807fd65c7ee9aa5bcc9e3d6c1
Maruthi Turaga <Maruthi.Turaga@phytel.com>
2016-03-23 16:27:17 +0000

    1821
    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Services;C20698

--- API/DataDomain/Phytel.API.DataDomain.Contact/Development/Current/Contact.Test/ContactTypeLookUpManagerTests.cs
+++ API/DataDomain/Phytel.API.DataDomain.Contact/Development/Current/Contact.Test/ContactTypeLookUpManagerTests.cs
@@ -1,13 +1,43 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Phytel.API.DataDomain.Contact.ContactTypeLookUp;
+using Phytel.API.DataDomain.Contact.DTO;
+using Phytel.API.DataDomain.Contact.MongoDB.DTO;
+using Phytel.API.Interface;
 
 namespace Phytel.API.DataDomain.Contact.Test
 {
     [TestClass]
     public class ContactTypeLookUpManagerTests
     {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
         public void ContactTypeLookUpManager_Null_Factory_Should_Throw()
         {
-            
+
+            var dataManager = new ContactTypeLookUpManager(null);
         }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void ContactTypeLookUpManager_Null_Repository_Should_Throw()
+        {
+            var mockFactory = new Mock<IContactTypeLookUpRepositoryFactory>();
+            
+
+            mockFactory.Setup(
+                f => f.GetContactTypeLookUpRepository(It.IsAny<IDataDomainRequest>(), It.IsAny<RepositoryType>()))
+                .Returns((IContactTypeLookUpRepository)null);
+           
+            var dataManager = new ContactTypeLookUpManager(mockFactory.Object);
+            var data = dataManager.GetContactTypeLookUps(It.IsAny<GetContactTypeLookUpDataRequest>());
+
+        }
+
+        
     }
 }
+ 
