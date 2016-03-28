d885fec31029974052f5aa8a81a0cc420bea4e39
Li Tan <HILLS\ltan>
2016-03-28 16:29:00 +0000

    ENG-1675 Schema Change
    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/Nightingale;C20745

---
+++ Reporting/Development/Current/Reporting/Scripts/Releases/HOTFIXES/ENG-1675/1.SchemaChange.sql
@@ -0,0 +1,16 @@
+alter table dbo.RPT_Patient
+alter column LSSN varchar(4)
+
+alter table dbo.RPT_PatientInformation
+alter column LSSN varchar(4)
+
+alter table dbo.RPT_Engage_Enrollment_Info
+alter column LSSN varchar(4)
+
+alter table dbo.RPT_CareBridge_Enrollment_Info
+alter column LSSN varchar(4)
+
+
+alter table dbo.RPT_BSHSI_HW2_Enrollment_Info
+alter column LSSN varchar(4)
+
