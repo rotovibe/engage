fdae64fc0482154aa3273788b10dd47bd3b21982
Li Tan <HILLS\ltan>
2016-03-28 16:29:16 +0000

    ENG-1675 Schema Change Rollback
    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/Nightingale;C20746

---
+++ Reporting/Development/Current/Reporting/Scripts/Releases/HOTFIXES/ENG-1675/1.SchemaChange_Rollback.sql
@@ -0,0 +1,17 @@
+
+alter table dbo.RPT_Patient
+alter column LSSN int
+
+alter table dbo.RPT_PatientInformation
+alter column LSSN int
+
+alter table dbo.RPT_Engage_Enrollment_Info
+alter column LSSN int
+
+alter table dbo.RPT_CareBridge_Enrollment_Info
+alter column LSSN int
+
+
+alter table dbo.RPT_BSHSI_HW2_Enrollment_Info
+alter column LSSN int
+
