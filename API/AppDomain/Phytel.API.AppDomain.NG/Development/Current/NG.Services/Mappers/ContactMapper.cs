fb8e81bf1a4e922ed302b7a7d28746e1dd792942
Maruthi Turaga <Maruthi.Turaga@phytel.com>
2016-03-29 19:59:58 +0000

    Update Mapper.
    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Services;C20792

--- API/AppDomain/Phytel.API.AppDomain.NG/Development/Current/NG.Services/Mappers/ContactMapper.cs
+++ API/AppDomain/Phytel.API.AppDomain.NG/Development/Current/NG.Services/Mappers/ContactMapper.cs
@@ -12,12 +12,31 @@ namespace Phytel.API.AppDomain.NG.Service.Mappers
     {
         public static void Build()
         {
-            Mapper.CreateMap<ContactData, Contact>().ReverseMap();            
-            Mapper.CreateMap<LanguageData, Language>();
-            Mapper.CreateMap<AddressData, Address>();
-            Mapper.CreateMap<EmailData, Email>();
-            Mapper.CreateMap<PhoneData, Phone>();
-            Mapper.CreateMap<CommModeData, CommMode>();
+            Mapper.CreateMap<ContactTypeData, ContactType>().ReverseMap();
+            Mapper.CreateMap<LanguageData, Language>().ReverseMap();
+            Mapper.CreateMap<AddressData, Address>().ReverseMap();
+            Mapper.CreateMap<EmailData, Email>().ReverseMap();
+            Mapper.CreateMap<PhoneData, Phone>().ReverseMap();
+
+            Mapper.CreateMap<CommModeData, CommMode>()
+                .ForMember(dest => dest.LookUpModeId, opt => opt.MapFrom(src => src.ModeId));
+
+            Mapper.CreateMap<CommMode, CommModeData>()
+               .ForMember(dest => dest.ModeId, opt => opt.MapFrom(src => src.LookUpModeId));
+
+
+
+            Mapper.CreateMap<ContactData, Contact>()
+                .ForMember(dest => dest.Type, opt => opt.MapFrom(src => src.TypeData))
+                .ForMember(dest => dest.IsPatient, opt => opt.Ignore())
+                .ForMember(dest => dest.IsUser, opt => opt.Ignore());
+
+            Mapper.CreateMap<Contact, ContactData>()
+                .ForMember(dest => dest.TypeData, opt => opt.MapFrom(src => src.Type))
+                .ForMember(dest => dest.RecentsList, opt => opt.Ignore())
+                .ForMember(dest => dest.CreatedOn, opt => opt.Ignore())
+                .ForMember(dest => dest.UpdatedOn, opt => opt.Ignore());
+
         }
     }
 }
