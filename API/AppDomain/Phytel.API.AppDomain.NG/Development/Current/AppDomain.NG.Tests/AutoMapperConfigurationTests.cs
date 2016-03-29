17f69ee44026e549381cf81cf56dbfa5bfddb97d
Maruthi Turaga <Maruthi.Turaga@phytel.com>
2016-03-29 19:46:17 +0000

    Add AutoMapper Configuration.
    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Services;C20791

---
+++ API/AppDomain/Phytel.API.AppDomain.NG/Development/Current/AppDomain.NG.Tests/AutoMapperConfigurationTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Phytel.API.AppDomain.NG.Service.Mappers;
+using Phytel.API.DataDomain.Patient.DTO;
+
+namespace Phytel.API.AppDomain.NG.Test
+{
+    [TestClass]
+    public class AutoMapperConfigurationTests
+    {
+        [TestInitialize]
+        public void SetUp()
+        {
+            ContactMapper.Build();
+        }
+
+        [TestMethod]
+        public void AssertAutoMapperConfigurationIsValid()
+        {
+            Mapper.AssertConfigurationIsValid();
+        }
+    }
+}
