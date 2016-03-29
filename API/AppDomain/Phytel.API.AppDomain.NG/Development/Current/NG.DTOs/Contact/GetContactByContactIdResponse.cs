a51b1ffe35725df132212ed2674d043e488e49fd
Maruthi Turaga <Maruthi.Turaga@phytel.com>
2016-03-29 18:24:32 +0000

    ENG-1890
    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Services;C20776

---
+++ API/AppDomain/Phytel.API.AppDomain.NG/Development/Current/NG.DTOs/Contact/GetContactByContactIdResponse.cs
@@ -0,0 +1,13 @@
+﻿using Phytel.API.Interface;
+using ServiceStack.ServiceInterface.ServiceModel;
+
+namespace Phytel.API.AppDomain.NG.DTO
+{
+    public class GetContactByContactIdResponse : IDomainResponse
+    {
+        public Contact Contact { get; set; }
+        public ResponseStatus Status { get; set; }
+        public double Version { get; set; }
+    }
+  
+}
