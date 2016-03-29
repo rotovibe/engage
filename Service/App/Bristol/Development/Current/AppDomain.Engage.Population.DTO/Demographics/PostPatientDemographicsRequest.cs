b73250481d3d4b9dcfac083474002ae3b5e614e1
Mel Bobadilla <mel.bobadilla@phytel.com>
2016-03-29 15:14:42 +0000

    added context to the appdomain
    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/Nightingale;C20768

--- Service/App/Bristol/Development/Current/AppDomain.Engage.Population.DTO/Demographics/PostPatientDemographicsRequest.cs
+++ Service/App/Bristol/Development/Current/AppDomain.Engage.Population.DTO/Demographics/PostPatientDemographicsRequest.cs
@@ -5,11 +5,15 @@ using ServiceStack.ServiceHost;
 
 namespace AppDomain.Engage.Population.DTO.Demographics
 {
-    [Route("/{Version}/{ContractNumber}/Patients/Demographics", "POST")]
+    [Route("/{Context}/{Version}/{ContractNumber}/Patients/Demographics", "POST")]
     public class PostPatientDemographicsRequest : IAppDomainRequest
     {
         [ApiMember(Name = "PatientList", Description = "Patients list", ParameterType = "Body", DataType = "List<Patient>", IsRequired = true)]
         public List<Patient> PatientList { get; set; }
+
+        // IAppDomainRequest implementation
+        [ApiMember(Name = "Context", Description = "Product Context", ParameterType = "property", DataType = "string", IsRequired = true)]
+        public string Context { get; set; }
 
         [ApiMember(Name = "Version", Description = "Name Description", ParameterType = "path", DataType = "string", IsRequired = true)]
         public double Version { get; set; }
