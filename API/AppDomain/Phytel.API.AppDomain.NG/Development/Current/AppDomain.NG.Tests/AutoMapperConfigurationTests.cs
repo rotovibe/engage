2fb72d9fcd0040b67a65d1b15dc2bbb6f3c6f196
Thierry Kalubi <Thierry.Kalubi2@phytel.com>
2016-03-31 20:25:42 +0000

    ENG-1821: Added unit test for ContactTypeLookUpsMapper.  Testing that the parentId is null when the Data Domain DTO has a parentId="000000000000000000000000"
    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Services;C20863

--- API/AppDomain/Phytel.API.AppDomain.NG/Development/Current/AppDomain.NG.Tests/AutoMapperConfigurationTests.cs
+++ API/AppDomain/Phytel.API.AppDomain.NG/Development/Current/AppDomain.NG.Tests/AutoMapperConfigurationTests.cs
@@ -5,7 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Phytel.API.AppDomain.NG.DTO;
 using Phytel.API.AppDomain.NG.Service.Mappers;
+using Phytel.API.DataDomain.Contact.DTO;
 using Phytel.API.DataDomain.Patient.DTO;
 
 namespace Phytel.API.AppDomain.NG.Test
@@ -17,12 +19,31 @@ namespace Phytel.API.AppDomain.NG.Test
         public void SetUp()
         {
             ContactMapper.Build();
+            ContactTypeLookUpsMappers.Build();
         }
 
         [TestMethod]
         public void AssertAutoMapperConfigurationIsValid()
         {
             Mapper.AssertConfigurationIsValid();
+        }
+
+        [TestMethod]
+        public void ContactTypeLookUpParentIdShouldBeNull_Test()
+        {
+            ContactTypeLookUpData cData = new ContactTypeLookUpData()
+            {
+                Id = "56ea0c2c64e91cf53bbfca5f",
+                Name = "Doctor",
+                Role = "Doctor(M.D)",
+                CreatedOn = DateTime.UtcNow,
+                Group = ContactLookUpGroupType.CareTeam,
+                ParentId = "000000000000000000000000"
+            };
+            var ct =  Mapper.Map<ContactTypeLookUp>(cData);
+            
+            Assert.IsNull(ct.ParentId);
+
         }
     }
 }
