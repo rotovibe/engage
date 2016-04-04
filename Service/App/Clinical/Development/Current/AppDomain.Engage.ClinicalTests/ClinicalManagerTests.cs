99c4cc99b23fb99f693427861b45aa5ea3a9a341
Mel Bobadilla <mel.bobadilla@phytel.com>
2016-04-04 21:59:13 +0000

    added test project
    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/Nightingale;C20974

---
+++ Service/App/Clinical/Development/Current/AppDomain.Engage.ClinicalTests/ClinicalManagerTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppDomain.Engage.Clinical;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace AppDomain.Engage.Clinical.Tests
+{
+    [TestClass()]
+    public class ClinicalManagerTests
+    {
+        [TestMethod()]
+        public void SavePatientMedicationsTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
