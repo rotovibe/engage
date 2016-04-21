a7a12cac7d0993c2bc63212b680eebc02f8b22ca
Maruthi Turaga <Maruthi.Turaga@phytel.com>
2016-04-21 15:23:43 +0000

    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Services;C21565

--- API/AppDomain/Phytel.API.AppDomain.NG/Development/Current/NG/Contact/CareTeam/ICohortCommand.cs
+++ API/AppDomain/Phytel.API.AppDomain.NG/Development/Current/NG/Contact/CareTeam/ICohortCommand.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Phytel.API.AppDomain.NG
+﻿namespace Phytel.API.AppDomain.NG
 {
     public interface ICohortCommand
     {
