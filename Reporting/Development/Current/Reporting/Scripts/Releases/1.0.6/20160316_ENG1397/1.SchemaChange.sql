b9f106cd79576ad0b7a0e6f3ca15b767edc98ef6
Li Tan <HILLS\ltan>
2016-04-22 20:10:43 +0000

    Delete no longer needed folder
    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/Nightingale;C21677

--- Reporting/Development/Current/Reporting/Scripts/Releases/1.0.6/20160316_ENG1397/1.SchemaChange.sql
+++
@@ -1,8 +0,0 @@
-
-Alter Table [dbo].[RPT_PatientObservation]
-Drop Column Type
-
-Alter Table [dbo].[RPT_Patient_ClinicalData]
-Drop Column Type
-
-
--- Reporting/Development/Current/Reporting/Scripts/Releases/1.0.6/20160316_ENG1397/1.SchemaChange_Rollback.sql
+++
@@ -1,6 +0,0 @@
-
-Alter Table [dbo].[RPT_PatientObservation]
-Add Type varchar(50)
-
-Alter Table [dbo].[RPT_Patient_ClinicalData]
-Add Type varchar(50)
