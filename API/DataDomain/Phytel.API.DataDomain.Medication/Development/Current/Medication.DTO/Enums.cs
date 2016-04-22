df25ef10d49913d71efe0f294c240e56f67e8f9a
Snehal Ingavale <Snehal.Ingavale@phytel.com>
2016-04-22 15:00:55 +0000

    Checking in https://jira.phytel.com/browse/ENG-1903 -  Added new statuses for Patient Allergies and Meds
    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Services;C21638

--- API/DataDomain/Phytel.API.DataDomain.Medication/Development/Current/Medication.DTO/Enums.cs
+++ API/DataDomain/Phytel.API.DataDomain.Medication/Development/Current/Medication.DTO/Enums.cs
@@ -4,7 +4,12 @@ namespace Phytel.API.DataDomain.Medication.DTO
     public enum Status
     {
         Active = 1,
-        Inactive = 2
+        Inactive = 2,
+        Refused = 3,
+        NotDoneMedical = 4, 
+        Unknown = 5,
+        Invalid = 6,
+        Duplicate = 7
     }
 
     public enum Category
