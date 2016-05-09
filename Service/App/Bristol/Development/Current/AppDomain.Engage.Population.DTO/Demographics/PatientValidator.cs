a31b026fcb7a683cd3f2d814496d89dc02c7fb34
Anusha Meena Dhanapalan <HILLS\adhanapalan>
2016-05-09 15:39:30 +0000

    validation for PCP
    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/Nightingale;C22295

--- Service/App/Bristol/Development/Current/AppDomain.Engage.Population.DTO/Demographics/PatientValidator.cs
+++ Service/App/Bristol/Development/Current/AppDomain.Engage.Population.DTO/Demographics/PatientValidator.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using ServiceStack.Common;
 using ServiceStack.FluentValidation;
+using System.Text.RegularExpressions;
 
 namespace AppDomain.Engage.Population.DTO.Demographics
 {
@@ -25,12 +26,26 @@ namespace AppDomain.Engage.Population.DTO.Demographics
             RuleFor(r => r.LastFourSSN).Length(4).Matches("^[0-9]*$").When(r => r.LastFourSSN.IsNullOrEmpty() == false);
             RuleFor(r => r.Gender).Length(1).Matches("^[M,F,O,m,f,o]*$").When(r=>r.Gender.IsNullOrEmpty() == false);
             RuleFor(r => r.MaritalStatusId).Length(1, 2).Matches("^[W|LS|LP|U]*$").When(r =>r.MaritalStatusId.IsNullOrEmpty() == false);
-            //RuleFor(r => r.ClinicalBackground)
-            //.Matches("{^[^|]+|[0-9]+|[^|]+;*$}")
-            //.When(r => r.ClinicalBackground.IsNullOrEmpty() == false);
+            RuleFor(r => r.ClinicalBackground)
+                .Must(ValidatePCPinfo)
+                .WithMessage("ClinicalBackground is invalid")
+                .When(r => r.ClinicalBackground.IsNullOrEmpty() == false);
 
         }
 
+        private bool ValidatePCPinfo(string pcpInfo)
+        {
+            String[] PCPstrings;
+            char[] charSeparators = new char[] { ';' };
+            PCPstrings = pcpInfo.Split(charSeparators, StringSplitOptions.RemoveEmptyEntries);
+            Regex rgx = new Regex(@"^[^|]*[|]{1}[0-9]{10}[|]{1}[^|]*$");
+            foreach(string s in PCPstrings)
+                if(!rgx.IsMatch(s)) return false;
+            return true;
+
+        
+
+        }
         
         
     }
