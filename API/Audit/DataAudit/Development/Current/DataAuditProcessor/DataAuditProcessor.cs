9398956b102824403cbc0230b33364837b623607
Man Tan Nguyen <HILLS\mnguyen>
2016-05-23 20:53:48 +0000

    Merge Medtronic DataAudit code with Engage.
    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/PhytelCode/Phytel.Net/Services;C22581

--- API/Audit/DataAudit/Development/Current/DataAuditProcessor/DataAuditProcessor.cs
+++ API/Audit/DataAudit/Development/Current/DataAuditProcessor/DataAuditProcessor.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Xml;
 using MongoDB.Driver;
 using Phytel.API.DataDomain.ASE.Common.Enums;
 using Phytel.API.DataDomain.ASE.DTO.Message;
@@ -9,6 +10,7 @@ namespace Phytel.API.DataAuditProcessor
     public class DataAuditProcessor: QueueProcessBase
     {
         string _DBConnName = "";
+        private const string ConfigXPathPrefix = "//Phytel.ASE.Process/ProcessConfiguration/";
 
         public override void Execute(QueueMessage queueMessage)
         {
@@ -20,12 +22,47 @@ namespace Phytel.API.DataAuditProcessor
 
                 MongoDatabase db = Phytel.Services.MongoService.Instance.GetDatabase(_DBConnName, da.Contract, true, "Audit");
                 db.GetCollection(da.EntityType).Insert(da);
+                
+                XmlNodeList queues = Configuration.SelectNodes(ConfigXPathPrefix + "AdditionalQueues/Queue");
+                if (queues != null)
+                {
+                    queueMessage.Routes.ForEach(t =>
+                    {
+                        t.RetryProcessInterval = GetRetryInterval();
+                        t.MaxProcessRetryCount = 4;
+                    });
+
+                    foreach (XmlNode queue in queues)
+                    {
+                        try
+                        {
+                            string queueName = queue.InnerText;
+                            RouteMessage(queueMessage, queueName);
+                        }
+                        catch (Exception ex)
+                        {
+                            LogError(ex, LogErrorCode.Error, LogErrorSeverity.Critical);
+                            RetryMessage = true;
+                        }
+                    }
+                }
             }
             catch(Exception ex)
             {
                 base.LogError(ex, LogErrorCode.Error, LogErrorSeverity.Critical);
                 throw;
             }
+        }
+
+        private int GetRetryInterval()
+        {
+            int retryInterval;
+            var interval = Configuration.SelectSingleNode(ConfigXPathPrefix + "RetryInterval");
+            if(interval != null && Int32.TryParse(interval.InnerText, out retryInterval))
+            {
+                return retryInterval;
+            }
+            return 1;
         }
     }
 }
