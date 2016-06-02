00b5893e320993d7029ba202f57caf15d8d8f8bc
Snehal Ingavale <Snehal.Ingavale@phytel.com>
2016-06-02 17:06:44 +0000

    Adding scripts for careteamfrequency
    git-tfs-id: [http://hillstfs2013:8080/tfs/Phytel]$/Nightingale;C22697

---
+++ Reporting/Development/Current/Reporting/Scripts/Releases/1.0.7/1_SchemaChanges.sql
@@ -0,0 +1,31 @@
+﻿----------------------------------------------------------------------------------------------------------------------------------
+--ENG-2010
+----------------------------------------------------------------------------------------------------------------------------------
+
+IF EXISTS (SELECT * FROM sys.objects WHERE type = 'U' AND name = 'RPT_CareTeamFrequency')
+	DROP TABLE [dbo].[RPT_CareTeamFrequency]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING OFF
+GO
+
+CREATE TABLE [dbo].[RPT_CareTeamFrequency](
+	[CareTeamFrequencyId] [int] IDENTITY(1,1) NOT NULL,
+	[MongoId] [varchar](50) NOT NULL,
+	[Name] [varchar](200) NULL,
+ CONSTRAINT [PK_CareTeamFrequency] PRIMARY KEY CLUSTERED 
+(
+	[CareTeamFrequencyId] ASC
+)
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
---
+++ Reporting/Development/Current/Reporting/Scripts/Releases/1.0.7/3_DataChanges.sql
@@ -0,0 +1,8 @@
+﻿----------------------------------------------------------------------------------------------------------------------------------
+--ENG-2010
+----------------------------------------------------------------------------------------------------------------------------------
+
+DELETE FROM RPT_SprocNames WHERE SprocName = 'spPhy_RPT_Flat_AssignedPCM';
+GO
+INSERT INTO RPT_SprocNames (SprocName, Prerequire) VALUES ('spPhy_RPT_Flat_AssignedPCM', 'false');
+GO
